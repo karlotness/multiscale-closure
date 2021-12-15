8c1dbebf5662a40ea0cadd93cec1b290ba56cd4f
Karl Otness <karl.otness@nyu.edu>
2021-12-15 11:58:53 -0500

    Include additional neural network activation functions.

--- src/methods/_defs.py
+++ src/methods/_defs.py
@@ -1,5 +1,10 @@
+import jax
+import jax.numpy as jnp
 import flax.linen as nn
 
 ACTIVATIONS = {
     "relu": nn.relu,
+    "hard_sigmoid": jax.nn.hard_sigmoid,
+    "tanh": jnp.tanh,
+    "sigmoid": nn.sigmoid,
 }
