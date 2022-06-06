a036695663d06995cf4797725932f81ef1624023
Karl Otness <karl.otness@nyu.edu>
2022-06-05 22:04:49 -0400

    Add copy of test architecture previously used in notebooks.
    This is similar to the Zanna-Bolton'20 architecture, but without the
    fixed convolutions in the last layer.

--- src/methods/__init__.py
+++ src/methods/__init__.py
@@ -1,1 +1,13 @@
+import importlib
 
+def make_module_factory(module, class_name):
+
+    def make_module():
+        mod_name = importlib.import_module(f".{module}", __name__)
+        return getattr(mod_name, class_name)()
+
+    return make_module
+
+ARCHITECTURES = {
+    "closure-cnn-v1": make_module_factory("cnn", "ClosureCnnV1")
+}
--- src/methods/cnn.py
+++ src/methods/cnn.py
@@ -16,3 +16,27 @@ class CNN(nn.Module):
             if i < len(self.features_kernels) - 1:
                 x = ACTIVATIONS[self.activation](x)
         return x
+
+
+class ClosureCnnV1(nn.Module):
+    @nn.compact
+    def __call__(self, u, v):
+        x = jnp.concatenate([u, v], axis=0)
+        x = jnp.moveaxis(x, 0, -1)
+        # Step 1
+        x = nn.Conv(features=256, kernel_size=(5, 5))(x)
+        x = jax.nn.selu(x)
+        # Step 2
+        x = nn.Conv(features=256, kernel_size=(5, 5))(x)
+        x = jax.nn.selu(x)
+        # Step 3
+        x = nn.Conv(features=128, kernel_size=(3, 3))(x)
+        x = jax.nn.selu(x)
+        # Step 4
+        x = nn.Conv(features=64, kernel_size=(3, 3))(x)
+        # No fixed convs
+        x = nn.Conv(features=2 * small_model.nz, kernel_size=(3, 3))(x)
+        x = jnp.moveaxis(x, -1, 0)
+        # Split and return
+        sx, sy = jnp.split(x, 2, axis=0)
+        return sx, sy
