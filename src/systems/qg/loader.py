e86bc153642651e7a8d8e7bd268337b3d394c34e
Karl Otness <karl.otness@nyu.edu>
2022-06-15 18:31:47 -0400

    Move loader to use asyncio to manage background processes.
    The hope is that this should reduce contention on the GIL and possibly
    provide some performance benefit. That is, there's only one background
    thread instead of one per process vying for time on the interpreter.
    We're mostly just shuffling output from pipes anyway.

--- src/systems/qg/loader.py
+++ src/systems/qg/loader.py
@@ -5,7 +5,7 @@ import pathlib
 import logging
 import queue
 import operator
-import subprocess
+import asyncio
 import contextlib
 import jax
 import jax.numpy as jnp
@@ -16,6 +16,10 @@ from .qg_model import QGModel
 
 
 __all__ = ["ThreadedQGLoader", "qg_model_from_hdf5"]
+
+
+_WORK_LOOP = None
+_WORK_LOOP_MUTEX = threading.Lock()
 
 
 def qg_model_from_hdf5(file_path, model="small"):
@@ -30,7 +34,45 @@ def _get_series_details(file_path, rollout_steps):
         return arr.dtype, arr.nbytes
 
 
-def _proc_worker_func(
+def _get_work_loop():
+    global _WORK_LOOP
+
+    def _start_loop(loop):
+        asyncio.set_event_loop(loop)
+        loop.run_forever()
+
+    with _WORK_LOOP_MUTEX:
+        if _WORK_LOOP is None:
+            _WORK_LOOP = asyncio.new_event_loop()
+            thread = threading.Thread(
+                target=_start_loop,
+                kwargs={
+                    "loop": _WORK_LOOP,
+                },
+                name="loader_worker",
+                daemon=True,
+            )
+            thread.start()
+        return _WORK_LOOP
+
+
+def _create_work_loop_cond(loop):
+
+    async def _create_cond():
+        return asyncio.Condition()
+
+    future = asyncio.run_coroutine_threadsafe(_create_cond(), loop)
+    return future.result()
+
+
+async def _signal_condition_cb(cond):
+    async with cond:
+        cond.notify()
+
+def _signal_work_loop_cond(loop, cond):
+    loop.call_soon_threadsafe(asyncio.create_task, _signal_condition_cb(cond))
+
+async def _proc_worker_task(
         in_queue,
         out_queue,
         logger,
@@ -41,41 +83,38 @@ def _proc_worker_func(
 ):
     logger.debug("process worker started")
     # Spawn worker's process
-    proc = subprocess.Popen(
-                [
-                    sys.executable,
-                    str(pathlib.Path(__file__).parent / "_loader.py"),
-                    str(file_path),
-                    f"{rollout_steps:d}",
-                ],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-            )
+    proc = await asyncio.create_subprocess_exec(
+        sys.executable,
+        str(pathlib.Path(__file__).parent / "_loader.py"),
+        str(file_path),
+        f"{rollout_steps:d}",
+        stdin=asyncio.subprocess.PIPE,
+        stdout=asyncio.subprocess.PIPE,
+    )
     logger.debug("spawned subprocess")
     try:
         while True:
-            job = in_queue.get()
+            job = await in_queue.get()
             if job is None:
                 # This is our stop signal
                 logger.debug("process worker signaled to stop")
                 return
             i, traj, step = job
             proc.stdin.write(f"{traj:d} {step:d}\n".encode("utf8"))
-            proc.stdin.flush()
-            out_queue.put((i, np.frombuffer(proc.stdout.read(arr_byte_size), dtype=arr_dtype)))
+            await proc.stdin.drain()
+            await out_queue.put((i, np.frombuffer(await proc.stdout.readexactly(arr_byte_size), dtype=arr_dtype)))
     except Exception:
         logger.exception("error in process worker")
         raise
     finally:
         proc.stdin.write(b"exit\n")
-        proc.stdin.flush()
-        return_code = proc.wait()
-        if return_code != 0:
+        await proc.stdin.drain()
+        if await proc.wait() != 0:
             logger.error("worker exited abnormally with code %d", return_code)
         else:
             logger.debug("stopped worker process")
 
-def _worker_func(
+async def _worker_coro(
         out_queue,
         logger,
         file_path,
@@ -85,34 +124,31 @@ def _worker_func(
         seed,
         num_trajs,
         num_steps,
+        arr_dtype,
+        arr_byte_size,
+        queue_wait_cond,
         num_procs=10,
 ):
     try:
         logger.debug("worker thread started")
-        arr_dtype, arr_byte_size = _get_series_details(file_path=file_path, rollout_steps=rollout_steps)
-        submit_queue = queue.SimpleQueue()
-        recieve_queue = queue.SimpleQueue()
+        submit_queue = asyncio.Queue()
+        recieve_queue = asyncio.Queue()
         sort_key_func = operator.itemgetter(0)
         undecorate_func = operator.itemgetter(1)
-        sub_threads = [
-            threading.Thread(
-                target=_proc_worker_func,
-                kwargs={
-                    "in_queue": submit_queue,
-                    "out_queue": recieve_queue,
-                    "logger": logger.getChild(f"proc{i}"),
-                    "file_path": file_path,
-                    "rollout_steps": rollout_steps,
-                    "arr_dtype": arr_dtype,
-                    "arr_byte_size": arr_byte_size,
-                },
-                name=f"loader_proc_worker_{i:d}",
-                daemon=True,
+        sub_tasks = [
+            asyncio.create_task(
+                _proc_worker_task(
+                    in_queue=submit_queue,
+                    out_queue=recieve_queue,
+                    logger=logger.getChild(f"proc{i}"),
+                    file_path=file_path,
+                    rollout_steps=rollout_steps,
+                    arr_dtype=arr_dtype,
+                    arr_byte_size=arr_byte_size,
+                )
             )
             for i in range(num_procs)
         ]
-        for thread in sub_threads:
-            thread.start()
         try:
             # Compute needed information
             rng = np.random.default_rng(seed=seed)
@@ -128,24 +164,30 @@ def _worker_func(
                 batch_trajs, batch_steps = np.divmod(samples, per_traj_valid_steps + 1)
                 # Assign each process a trajectory
                 for i, (traj, step) in enumerate(zip(batch_trajs, batch_steps, strict=True)):
-                    submit_queue.put((i, traj, step))
+                    await submit_queue.put((i, traj, step))
                 # Retrieve results
                 arr_stack = []
                 for _i in range(batch_size):
-                    arr_stack.append(recieve_queue.get())
+                    arr_stack.append(await recieve_queue.get())
                 arr_stack.sort(key=sort_key_func)
                 # Stack and split arrays, push to GPU and place in queue
                 arr_stack = np.stack(list(map(undecorate_func, arr_stack)))
-                out_queue.put(kernel.PseudoSpectralKernelState(**{k: jax.device_put(arr_stack[k]) for k in data_fields}))
+                out_result = kernel.PseudoSpectralKernelState(**{k: jax.device_put(arr_stack[k]) for k in data_fields})
+                while True:
+                    async with queue_wait_cond:
+                        try:
+                            out_queue.put_nowait(out_result)
+                            break
+                        except queue.Full:
+                            await queue_wait_cond.wait()
         except Exception:
             logger.exception("exception inside worker thread, terminating worker and processes")
             raise
         finally:
             # Signal our workers to stop
             for _i in range(num_procs):
-                submit_queue.put(None)
-            for thread in sub_threads:
-                thread.join()
+                await submit_queue.put(None)
+            await asyncio.gather(*sub_tasks, return_exceptions=True)
             logger.debug("finished stopping all workers")
     except Exception:
         logger.exception("exception inside worker thread, terminating worker")
@@ -190,25 +232,31 @@ class ThreadedQGLoader:
         self.num_steps = num_steps
         self.rollout_steps = rollout_steps
         self.batch_size = batch_size
-
-        self._worker_thread = threading.Thread(
-            target=_worker_func,
-            kwargs={
-                "out_queue": self._queue,
-                "logger": self._logger.getChild("worker"),
-                "file_path": file_path,
-                "stop_event": self._stop_event,
-                "batch_size": batch_size,
-                "rollout_steps": rollout_steps,
-                "seed": seed,
-                "num_trajs": num_trajs,
-                "num_steps": num_steps,
-                "num_procs": num_workers,
-            },
-            name=f"{logger_name}_worker",
-            daemon=True,
+        self._arr_dtype, self._arr_byte_size = _get_series_details(
+            file_path=file_path,
+            rollout_steps=self.rollout_steps
         )
-        self._worker_thread.start()
+
+        self._work_loop = _get_work_loop()
+        self._queue_wait_cond = _create_work_loop_cond(self._work_loop)
+        self._worker_future = asyncio.run_coroutine_threadsafe(
+            _worker_coro(
+                out_queue=self._queue,
+                logger=self._logger.getChild("worker"),
+                file_path=file_path,
+                stop_event=self._stop_event,
+                batch_size=batch_size,
+                rollout_steps=rollout_steps,
+                seed=seed,
+                num_trajs=num_trajs,
+                num_steps=num_steps,
+                arr_dtype=self._arr_dtype,
+                arr_byte_size=self._arr_byte_size,
+                queue_wait_cond=self._queue_wait_cond,
+                num_procs=num_workers,
+            ),
+            self._work_loop
+        )
 
     def __enter__(self):
         return self
@@ -224,7 +272,9 @@ class ThreadedQGLoader:
     def next_batch(self):
         if self._stop_event.is_set():
             raise ValueError("Closed dataset, cannot load batches")
-        return self._queue.get()
+        res = self._queue.get()
+        _signal_work_loop_cond(self._work_loop, self._queue_wait_cond)
+        return res
 
     def iter_batches(self):
         # A generator over the batches
@@ -238,10 +288,12 @@ class ThreadedQGLoader:
         try:
             while True:
                 self._queue.get_nowait()
+                _signal_work_loop_cond(self._work_loop, self._queue_wait_cond)
         except queue.Empty:
             # Ignore the exception now that queue is empty
             pass
-        self._worker_thread.join()
+        # Join the worker
+        self._worker_future.result()
 
 
 class SimpleQGLoader:
