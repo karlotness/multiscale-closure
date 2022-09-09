0ce69402cfc01064cfae0a2385c7b6116ba33cb1
Karl Otness <karl.otness@nyu.edu>
2022-09-09 17:52:35 -0400

    Allow passing arguments to module construction.

--- src/methods/__init__.py
+++ src/methods/__init__.py
@@ -2,9 +2,9 @@ import importlib
 
 def make_module_factory(module, class_name):
 
-    def make_module():
+    def make_module(*args, **kwargs):
         mod_name = importlib.import_module(f".{module}", __name__)
-        return getattr(mod_name, class_name)()
+        return getattr(mod_name, class_name)(*args, **kwargs)
 
     return make_module
 
