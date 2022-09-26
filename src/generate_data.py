666ea31724a674ecccb833a6b88a84da26091910
Karl Otness <karl.otness@nyu.edu>
2022-09-26 17:10:05 -0400

    Add ability generate single snapshots at the end of a trajectory.

--- src/generate_data.py
+++ src/generate_data.py
@@ -28,6 +28,17 @@ parser_qg.add_argument("--small_size", type=int, default=64, help="Scale of smal
 parser_qg.add_argument("--num_trajs", type=int, default=1, help="Number of trajectories to generate")
 parser_qg.add_argument("--config", type=str, default="eddy", help="Eddy or jet configuration", choices=["eddy", "jet"])
 
+# QG options
+parser_qg = subparsers.add_parser("qg_snap", help="Generate training data like PyQG")
+parser_qg.add_argument("seed", type=int, help="RNG seed, must be unique for unique trajectory")
+parser_qg.add_argument("--dt", type=float, default=3600.0, help="Time step size")
+parser_qg.add_argument("--tmax", type=float, default=155520000.0, help="End time for the model")
+parser_qg.add_argument("--big_size", type=int, default=256, help="Scale of large model")
+parser_qg.add_argument("--small_size", type=int, default=64, help="Scale of small model")
+parser_qg.add_argument("--num_trajs", type=int, default=1, help="Number of trajectories to generate")
+parser_qg.add_argument("--batch_size", type=int, default=100, help="Number of generation steps to batch together")
+parser_qg.add_argument("--config", type=str, default="eddy", help="Eddy or jet configuration", choices=["eddy", "jet"])
+
 CONFIG_VARS = {
     "eddy": {
         "rek": 5.787e-7,
@@ -74,6 +85,88 @@ def make_generate_coarse_traj(coarse_op, num_steps):
         return q, t, tc, ablevel, dqhdt
 
     return make_traj
+
+
+def make_generate_final_step(big_model, coarse_ops, num_steps):
+
+    def step_forward(carry, _x):
+        prev_big_state = carry
+        next_big_state = big_model.step_forward(prev_big_state)
+        return next_big_state, None
+
+    def make_traj(rng):
+        big_initial_step = big_model.create_initial_state(rng)
+        final_state, _ys = jax.lax.scan(step_forward, big_initial_step, None, length=num_steps - 1)
+        # Coarsen the final state
+        coarsened = {
+            op_name: op._coarsen_step(final_state).q
+            for op_name, op in coarse_ops.items()
+        }
+        return coarsened
+
+    return make_traj
+
+
+def chunk_array(arr, chunk, axis=0):
+    if chunk < 1:
+        raise ValueError(f"invalid array chunk size {chunk}")
+    return jnp.split(arr, jnp.arange(chunk, arr.shape[axis], chunk), axis=axis)
+
+
+def gen_qg_final_step(out_dir, args, base_logger):
+    out_dir = pathlib.Path(out_dir)
+    logger = base_logger.getChild("qg")
+    logger.info("Generating final step snapshots for QG with seed %d", args.seed)
+    # Initialize models
+    if args.config == "jet":
+        raise ValueError("jet configuration is currently broken")
+    big_model = QGModel(nx=args.big_size, ny=args.big_size, dt=args.dt, tmax=args.tmax, **CONFIG_VARS[args.config])
+    # Initialize coarsening operators
+    coarsen_operators = {
+        "op1": coarsen.Operator1(big_model=big_model, small_nx=args.small_size),
+        "op2": coarsen.Operator2(big_model=big_model, small_nx=args.small_size),
+    }
+    # Set up data generator
+    rng_ctr = jax.random.PRNGKey(seed=args.seed)
+    rng_ctr, step_rng = jnp.split(jax.random.split(rng_ctr, num=args.num_trajs + 1), [1])
+    # Do computations
+    num_steps = math.ceil(args.tmax / args.dt)
+    gen_func = jax.jit(jax.vmap(make_generate_final_step(big_model=big_model, coarse_ops=coarsen_operators, num_steps=num_steps)))
+    # Create output files and directories
+    op_directories = {}
+    for op_name in coarsen_operators.keys():
+        op_directories[op_name] = out_dir / op_name
+        op_directories[op_name].mkdir(exist_ok=True)
+    with contextlib.ExitStack() as exit_stack:
+        # Create output files
+        op_files = {
+            op_name: exit_stack.enter_context(h5py.File(op_directories[op_name] / "data.hdf5", "w", libver="latest"))
+            for op_name in coarsen_operators.keys()
+        }
+        # Store model parameters in each file
+        for op_name, op in coarsen_operators.items():
+            out_file = op_files[op_name]
+            param_group = out_file.create_group("params")
+            param_group.create_dataset("big_model", data=big_model.param_json())
+            param_group.create_dataset("small_model", data=op.small_model.param_json())
+            param_group.create_dataset("num_steps", data=num_steps)
+        # Create a dataset in each file
+        op_datasets = {}
+        for op_name in coarsen_operators.keys():
+            # Get dtype
+            dummy_q = coarsen_operators[op_name].small_model.create_initial_state(jax.random.PRNGKey(0)).q
+            op_datasets[op_name] = op_files[op_name].create_dataset("final_q_steps", shape=(args.num_trajs, ) + dummy_q.shape, dtype=dummy_q.dtype)
+        # Create the dataset in the output file
+        for batch_num, rng_chunk in enumerate(chunk_array(step_rng, chunk=args.batch_size)):
+            idx_start = args.batch_size * batch_num
+            idx_end = min(args.batch_size * (batch_num + 1), args.num_trajs)
+            logger.info("Starting batch %d with trajectories %d to %d of %d", batch_num, idx_start, idx_end, args.num_trajs)
+            final_q_steps = jax.device_get(gen_func(rng_chunk))
+            logger.info("Finished batch %d", batch_num)
+            # Store results
+            for op_name in coarsen_operators.keys():
+                op_files[op_name]["final_q_steps"][idx_start:idx_end] = final_q_steps[op_name]
+            logger.info("Finished storing batch %d", batch_num)
 
 
 def gen_qg(out_dir, args, base_logger):
@@ -158,8 +251,11 @@ if __name__ == "__main__":
             git_info.hash,
             "clean" if git_info.clean_worktree else "dirty"
         )
-    if args.system == "qg":
-        gen_qg(out_dir, args, logger)
-    else:
-        raise ValueError(f"invalid system: {args.system}")
+    match args.system:
+        case "qg":
+            gen_qg(out_dir, args, logger)
+        case "qg_snap":
+            gen_qg_final_step(out_dir, args, logger)
+        case _:
+            raise ValueError(f"invalid system: {args.system}")
     logger.info("Finished generating data")
