06bc0d450f489a7ff1c7772b71a366feff9d8efb
Karl Otness <karl.otness@nyu.edu>
2022-10-20 07:29:59 -0400

    Add initial training loop for diffrax-based SDE sampler.

--- src/train_sdegm.py
+++ src/train_sdegm.py
@@ -10,11 +10,9 @@ import h5py
 import json
 import jax
 import jax.numpy as jnp
+import diffrax
+import equinox as eqx
 import optax
-import flax.serialization
-import flax.training.train_state
-import flax.struct
-from flax.core import frozen_dict
 import numpy as np
 import logging
 import time
@@ -23,16 +21,10 @@ from typing import Callable
 from systems.qg.qg_model import QGModel
 from systems.qg import utils as qg_utils
 from systems.qg.loader import ThreadedQGLoader, SimpleQGLoader, qg_model_from_hdf5
+from methods.unet import UNet
 import jax_utils
 import utils
-from methods import ARCHITECTURES
-from methods.score_sde.models import utils as mutils
-from methods.score_sde import losses as losses_module
-from methods.score_sde import sampling
-from methods.score_sde.configs.dummy_qg_snap_config import build_dummy_qg_snap_config
-from methods.score_sde import datasets, sde_lib
 
-MEAN_QG_VAL_VALUE = 2.716908e-07
 
 parser = argparse.ArgumentParser(description="Train neural networks for closure")
 parser.add_argument("out_dir", type=str, help="Directory to store output (created if non-existing)")
@@ -40,12 +32,14 @@ parser.add_argument("train_set", type=str, help="Directory with training example
 parser.add_argument("--log_level", type=str, help="Level for logger", default="info", choices=["debug", "info", "warning", "error", "critical"])
 parser.add_argument("--save_interval", type=int, default=1, help="Number of epochs between saves")
 parser.add_argument("--seed", type=int, default=None, help="Seed to use with RNG (if None, select automatically)")
-parser.add_argument("--architecture", type=str, default="sdegm-ncsnpp", help="Choose architecture to train", choices=sorted(ARCHITECTURES.keys()))
 parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
 parser.add_argument("--num_epochs", type=int, default=100, help="Number of training epochs")
 parser.add_argument("--batches_per_epoch", type=int, default=100, help="Training batches per epoch")
 parser.add_argument("--lr", type=float, default=1e-5, help="Learning rate for optimizer")
+parser.add_argument("--weight_decay", type=float, default=0, help="Weight decay")
+parser.add_argument("--dt", type=float, default=0.01, help="Time step size when running diffusion")
 parser.add_argument("--num_epoch_samples", type=int, default=15, help="Number of samples to draw after each epoch")
+parser.add_argument("--num_hutch_samples", type=int, default=100, help="Number of samples to use when estimating the Jacobian")
 
 
 def save_network(output_name, output_dir, state, base_logger=None):
@@ -54,103 +48,132 @@ def save_network(output_name, output_dir, state, base_logger=None):
     else:
         logger = base_logger.getChild("save")
     output_dir = pathlib.Path(output_dir)
-    with open(output_dir / f"{output_name}.flaxnn.PART", "wb") as out_file:
-        out_file.write(
-            flax.serialization.to_bytes(
-                frozen_dict.freeze(
-                    {
-                        "params": state.params,
-                        "ema_rate": state.ema_rate,
-                        "params_ema": state.params_ema,
-                        "model_state": state.model_state,
-                    }
-                )
-            )
-        )
+    eqx.tree_serialise_leaves(output_dir / f"{output_name}.eqx.PART", state.net)
     try:
-        os.remove(output_dir / f"{output_name}.flaxnn")
+        os.remove(output_dir / f"{output_name}.eqx")
     except FileNotFoundError:
         pass
-    os.rename(output_dir / f"{output_name}.flaxnn.PART", output_dir / f"{output_name}.flaxnn")
+    os.rename(output_dir / f"{output_name}.eqx.PART", output_dir / f"{output_name}.eqx")
     logger.info("Saved network parameters to %s in %s", output_name, output_dir)
 
 
+def make_ou_solver(dt, t0=0.0, t1=1.0):
+    num_steps = math.floor((t1 - t0) / dt)
 
-def make_epoch_computer(sde, score_model, optimize_fn, reduce_mean, continuous, likelihood_weighting, scaler, inverse_scaler, num_steps, batch_size, train_data):
-    train_step_fn = losses_module.get_step_fn(sde, score_model, train=True, optimize_fn=optimize_fn,
-                                       reduce_mean=reduce_mean, continuous=continuous,
-                                       likelihood_weighting=likelihood_weighting)
+    def drift(t, y, args):
+        return -y
 
-    def do_batch(carry, _x):
-        rng_ctr, state = carry
-        rng, rng_ctr = jax.random.split(rng_ctr)
-        batch_idx = jax.random.randint(rng, (batch_size, ), 0, train_data.shape[0], dtype=jnp.uint32)
-        batch = jnp.take(train_data, batch_idx, axis=0)
-        batch = {"image": scaler(batch)}
-        (_, new_state), loss = train_step_fn((rng, state), batch)
-        # Guard against NaN losses, keep old state
-        out_state = jax.lax.cond(
-            jnp.isfinite(loss),
-            lambda: new_state,
-            lambda: state,
+    def diffuse_control(t, y, args):
+        return math.sqrt(2)
+
+    def get_steps(snapshot, rng):
+        brownian = diffrax.UnsafeBrownianPath(shape=snapshot.shape, key=rng)
+        control_noise = diffrax.WeaklyDiagonalControlTerm(diffuse_control, brownian)
+        terms = diffrax.MultiTerm(diffrax.ODETerm(drift), control_noise)
+        solver = diffrax.Euler()
+        saveat = diffrax.SaveAt(steps=True)
+        solve = diffrax.diffeqsolve(terms, solver, t0, t1, dt0=dt, y0=snapshot, saveat=saveat, adjoint=diffrax.NoAdjoint())
+        data = solve.ys[:num_steps]
+        times = solve.ts[:num_steps]
+        return jnp.concatenate([jnp.expand_dims(snapshot, 0), data]), jnp.concatenate([jnp.expand_dims(jnp.zeros_like(times[0]), 0), times])
+
+    return get_steps
+
+
+def make_epoch_computer(dt, batch_size, train_data, num_steps, num_hutch_samples):
+
+    def sample_loss(snapshot, t, rng, net):
+        net_rng, hutch_rng = jax.random.split(rng, 2)
+        orig_shape = snapshot.shape
+        est_trace_jac = jax_utils.trace_jac_hutch(
+            lambda x: net(x.reshape(orig_shape), t, key=net_rng).ravel(),
+            snapshot.ravel(),
+            rng=hutch_rng,
+            num_samples=num_hutch_samples,
         )
-        return (rng_ctr, out_state), loss
+        est_norm = jnp.sum(net(snapshot, t, key=net_rng)**2) / 2
+        return est_trace_jac + est_norm
+
+    def batch_loss(net, snapshots, ts, rng):
+        n_batch = snapshots.shape[0]
+        rngs = jnp.stack(jax.random.split(rng, n_batch))
+        losses = jax.vmap(functools.partial(sample_loss, net=net))(snapshots, ts, rngs)
+        return jnp.mean(losses)
+
+    def batch_sde(snapshots, rng):
+        n_batch = snapshots.shape[0]
+        rngs = jnp.stack(jax.random.split(rng, n_batch))
+        ode_fwd = make_ou_solver(dt=dt, t0=0.0, t1=1.0)
+        ys, ts = jax.vmap(ode_fwd)(snapshots, rngs)
+        return ys, ts
+
+    def do_batch(carry, _x, m_fixed):
+        rng_ctr, m_vary = carry
+        state = eqx.combine(m_vary, m_fixed)
+        rng_batch, rng_sde, rng_times, rng_loss, rng_ctr = jax.random.split(rng_ctr, 5)
+        batch_idx = jax.random.randint(rng_batch, (batch_size, ), 0, train_data.shape[0], dtype=jnp.uint32)
+        batch = jnp.take(train_data, batch_idx, axis=0)
+        # Run the ou process
+        snaps, ts = batch_sde(batch, rng_sde)
+        # Select times
+        time_idx = jax.random.randint(rng_times, (batch_size, 1), 0, ts.shape[1], dtype=jnp.uint32)
+        snaps = jax.vmap(functools.partial(jnp.take, axis=0))(snaps, time_idx).squeeze(axis=1)
+        ts = jax.vmap(functools.partial(jnp.take, axis=0))(ts, time_idx)
+        # Compute loss
+        loss, grads = eqx.filter_value_and_grad(batch_loss)(state.net, snaps, ts, rng=rng_loss)
+        # Update parameters
+        out_state = state.apply_updates(grads)
+        out_state_vary, _out_state_fixed = eqx.partition(state, eqx.is_array)
+        return (rng_ctr, out_state_vary), loss
 
     def do_epoch(state, rng):
-        (last_rng, new_state), losses = jax.lax.scan(do_batch, (rng, state), None, length=num_steps)
+        m_vary, m_fixed = eqx.partition(state, eqx.is_array)
+        (last_rng, new_vary), losses = jax.lax.scan(
+            functools.partial(do_batch, m_fixed=m_fixed),
+            (rng, m_vary),
+            None,
+            length=num_steps
+        )
+        new_state = eqx.combine(new_vary, m_fixed)
         return new_state, last_rng, losses
 
     return do_epoch
 
 
-def make_sample_computer(config, sde, score_model, inverse_scaler, sampling_eps, num_epoch_samples, data_mean, data_std):
-
-    sampling_shape = (num_epoch_samples, config.data.image_size,
-                      config.data.image_size, config.data.num_channels)
-    sampling_fn = sampling.get_sampling_fn(config, sde, score_model, sampling_shape, inverse_scaler, sampling_eps)
-
-    def do_sample(rng, state):
-        sample, nfe = sampling_fn(rng, state)
-        sample = (sample * data_std) + data_mean
-        return sample, nfe
-
-    return do_sample
-
-
-
-def init_network(model_name, config, rng):
-    model_def = functools.partial(ARCHITECTURES[model_name], config=config)
-    input_shape = (config.training.batch_size, config.data.image_size, config.data.image_size, config.data.num_channels)
-    label_shape = (1, )
-    fake_input = jnp.zeros(input_shape)
-    fake_label = jnp.zeros(label_shape, dtype=jnp.int32)
-    params_rng, dropout_rng, rng = jax.random.split(rng, 3)
-    model = model_def()
-    variables = model.init({'params': params_rng, 'dropout': dropout_rng}, fake_input, fake_label)
-    # Variables is a `flax.FrozenDict`. It is immutable and respects functional programming
-    init_model_state, initial_params = variables.pop('params')
-    # So far we have model, init_model_state, initial_params
-    optimizer = losses_module.get_optimizer(config)
-    optim_state = optimizer.init(initial_params)
-    state = mutils.State(
-        lr=config.optim.lr,
-        ema_rate=config.model.ema_rate,
-        model_state=init_model_state,
-        params_ema=initial_params,
-        rng=rng,
-        step=0,
-        apply_fn=model.apply,
-        params=initial_params,
-        tx=optimizer,
-        opt_state=optim_state,
+def init_network(lr, weight_decay, rng):
+    net = UNet(key=rng)
+    if weight_decay != 0:
+        optim = optax.adamw(learning_rate=lr, weight_decay=weight_decay)
+    else:
+        optim = optax.adam(learning_rate=lr)
+    state = jax_utils.EquinoxTrainState(
+        net=net,
+        optim=optim,
     )
-    return model, state
-    # mutils.State(step=0, optimizer=optimizer, lr=config.optim.lr,
-    #                      model_state=init_model_state,
-    #                      ema_rate=config.model.ema_rate,
-    #                      params_ema=initial_params,
-    #                      rng=rng)  # pytype: disable=wrong-keyword-args
-    # return model, state
+    return state
+
+
+def make_scalers(data):
+    assert data.ndim == 4
+    assert data.shape[1] == 2
+    assert data.shape[-1] == data.shape[-2]
+    mean = np.expand_dims(np.asarray(jnp.mean(data, axis=(0, -1, -2))), (-1, -2))
+    std = np.expand_dims(np.asarray(jnp.std(data, axis=(0, -1, -2))), (-1, -2))
+
+    def scaler(snapshot):
+        return (snapshot - mean) / std
+
+    def unscaler(snapshot):
+        return (snapshot * std) + mean
+
+    def scale_deriv(deriv):
+        return deriv / std
+
+    return scaler, unscaler, scale_deriv, {
+        "mean": tuple(mean.squeeze()),
+        "std": tuple(std.squeeze()),
+    }
+
 
 def main():
     args = parser.parse_args()
@@ -174,16 +197,7 @@ def main():
     else:
         seed = args.seed
     logger.info("Using seed %d", seed)
-    if jax.device_count() != 1:
-        logger.error("Cannot handle multiple JAX devices, found %d", jax.device_count())
-        raise ValueError(f"Too many JAX devices {jax.device_count()}")
-    # Build dummy config
-    config = build_dummy_qg_snap_config(
-        batch_size=args.batch_size,
-        num_epochs=args.num_epochs,
-        batches_per_epoch=args.batches_per_epoch,
-        lr=args.lr,
-    )
+
     # Configure required elements for training
     rng_ctr = jax.random.PRNGKey(seed=seed)
     train_path = pathlib.Path(args.train_set) / "data.hdf5"
@@ -194,83 +208,43 @@ def main():
     samples_dir.mkdir(exist_ok=True)
     # Construct neural net
     rng, rng_ctr = jax.random.split(rng_ctr, 2)
-    logger.info("Training network %s", args.architecture)
-    score_model, state = init_network(
-        model_name=args.architecture,
-        config=config,
+    logger.info("Training network: unet")
+    state = init_network(
+        lr=args.lr,
+        weight_decay=args.weight_decay,
         rng=rng,
     )
-    initial_step = 0
-    rng = state.rng
 
     # Load dataset
     logger.info("Loading data")
     with h5py.File(train_path, "r") as train_file:
         train_data = train_file["final_q_steps"][:]
     # Mask out NaNs, put batches at the back, and move to GPU
-    train_data = jax.device_put(np.moveaxis(train_data[np.all(np.isfinite(train_data), axis=(-1, -2, -3))], 1, -1))
-    # Rescale data to [0, 1]
-    train_mean = jnp.mean(train_data)
-    train_std = jnp.std(train_data)
-    train_data = (train_data - train_mean) / train_std
-    logger.info("Finished loading data, mean=%g, std=%g", train_mean, train_std)
+    train_data = jax.device_put(train_data[np.all(np.isfinite(train_data), axis=(-1, -2, -3))])
     # Create data normalizer and its inverse
-    scaler = datasets.get_data_scaler(config)
-    inverse_scaler = datasets.get_data_inverse_scaler(config)
+    scaler, unscaler, scale_deriv, data_stats = make_scalers(train_data)
+    logger.info("Finished loading data, mean=(%g, %g), std=(%g, %g)", data_stats["mean"][0], data_stats["mean"][1], data_stats["std"][0], data_stats["std"][1])
+    # Rescale data to be closer to standard normal per-channel
+    train_data = jax.vmap(scaler)(train_data)
 
-    # Setup SDE
-    if config.training.sde.lower() == 'vpsde':
-        sde = sde_lib.VPSDE(beta_min=config.model.beta_min, beta_max=config.model.beta_max, N=config.model.num_scales)
-        sampling_eps = 1e-3
-    elif config.training.sde.lower() == 'subvpsde':
-        sde = sde_lib.subVPSDE(beta_min=config.model.beta_min, beta_max=config.model.beta_max, N=config.model.num_scales)
-        sampling_eps = 1e-3
-    elif config.training.sde.lower() == 'vesde':
-        sde = sde_lib.VESDE(sigma_min=config.model.sigma_min, sigma_max=config.model.sigma_max, N=config.model.num_scales)
-        sampling_eps = 1e-5
-    else:
-        raise NotImplementedError(f"SDE {config.training.sde} unknown.")
-
-    # Build one-step training and evaluation functions
-    optimize_fn = losses_module.optimization_manager(config)
-    continuous = config.training.continuous
-    reduce_mean = config.training.reduce_mean
-    likelihood_weighting = config.training.likelihood_weighting
-    # TRAINING FUNCTIONS
-    train_epoch_fn = jax.jit(
+    # Training functions
+    train_epoch_fn = eqx.filter_jit(
         make_epoch_computer(
-            sde=sde,
-            score_model=score_model,
-            optimize_fn=optimize_fn,
-            reduce_mean=reduce_mean,
-            continuous=continuous,
-            likelihood_weighting=likelihood_weighting,
-            scaler=scaler,
-            inverse_scaler=inverse_scaler,
-            num_steps=args.batches_per_epoch,
+            dt=args.dt,
             batch_size=args.batch_size,
-            train_data=train_data
-        )
-    )
-    sample_fn = jax.jit(
-        make_sample_computer(
-            config=config,
-            sde=sde,
-            score_model=score_model,
-            inverse_scaler=inverse_scaler,
-            sampling_eps=sampling_eps,
-            num_epoch_samples=args.num_epoch_samples,
-            data_mean=train_mean,
-            data_std=train_std,
+            train_data=train_data,
+            num_steps=args.batches_per_epoch,
+            num_hutch_samples=args.num_hutch_samples,
         )
     )
 
     min_mean_loss = None
-    rng, rng_ctr = jax.random.split(rng_ctr, 2)
+    train_rng, rng_ctr = jax.random.split(rng_ctr, 2)
     for epoch in range(args.num_epochs):
+        # Training
         logger.info("Starting epoch %d of %d", epoch + 1, args.num_epochs)
         epoch_start = time.perf_counter()
-        state, rng, losses = train_epoch_fn(state, rng)
+        state, train_rng, losses = train_epoch_fn(state, train_rng)
         mean_loss = jax.device_get(jnp.mean(losses))
         final_loss = jax.device_get(losses[-1])
         epoch_end = time.perf_counter()
@@ -278,28 +252,14 @@ def main():
         logger.info("Epoch %d mean loss %f", epoch + 1, mean_loss)
         logger.info("Epoch %d final loss %f", epoch + 1, final_loss)
 
+        # Validation
+
         # Save weights
         if min_mean_loss is None or (np.isfinite(mean_loss) and mean_loss <= min_mean_loss):
             min_mean_loss = mean_loss
             save_network("best_loss", output_dir=weights_dir, state=state, base_logger=logger)
         if epoch % args.save_interval == 0:
-            save_network("epoch", output_dir=weights_dir, state=state, base_logger=logger)
-
-        # Do epoch sampling
-        logger.info("Starting sampling")
-        rng, sample_rng = jax.random.split(rng)
-        sample_start = time.perf_counter()
-        sample, nfe = jax.device_get(sample_fn(sample_rng, state))
-        sample_end = time.perf_counter()
-        sample = np.moveaxis(sample, -1, 1)
-        logger.info("Finished sampling in %f sec", sample_end - sample_start)
-        sample_min = np.min(sample)
-        sample_max = np.max(sample)
-        sample_mean = np.mean(sample)
-        logger.info("Sample stats: min=%g, max=%g, mean=%g", sample_min, sample_max, sample_mean)
-        epoch_sample_path = samples_dir / f"samples_ep{epoch + 1:05d}.npz"
-        np.savez(epoch_sample_path, sample=sample, nfe=nfe)
-        logger.info("Saved samples to %s", epoch_sample_path)
+            save_network("interval", output_dir=weights_dir, state=state, base_logger=logger)
 
 
 if __name__ == "__main__":
