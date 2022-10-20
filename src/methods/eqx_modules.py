824b18238aa97b51a9b46035a6ee2277cdcf332f
Karl Otness <karl.otness@nyu.edu>
2022-10-20 03:12:03 -0400

    Add extended equinox convolution module capable of circular padding.

---
+++ src/methods/eqx_modules.py
@@ -0,0 +1,80 @@
+from collections.abc import Sequence
+import typing
+from jaxtyping import Array
+import jax.numpy as jnp
+import equinox as eqx
+
+def _do_pad_input(x, pad_type, strides, filter_sizes, n_spatial_dims, dilations):
+    if pad_type not in {"valid", "circular", "same"}:
+        raise ValueError(f"invalid padding type {pad_type}")
+    if pad_type == "valid":
+        return x
+    # Compute padding size for same/circular
+    spatial_dims = x.shape[-n_spatial_dims:]
+    if isinstance(strides, int):
+        strides = (strides, ) * n_spatial_dims
+    if isinstance(filter_sizes, int):
+        filter_sizes = (filter_sizes, ) * n_spatial_dims
+    if isinstance(dilations, int):
+        dilations = (dilations, ) * n_spatial_dims
+    pad_amts = [(0, 0)] * (x.ndim - n_spatial_dims)
+    for stride, filt, size, dilate in zip(strides, filter_sizes, spatial_dims, dilations):
+        filt = filt * dilate
+        if size % stride == 0:
+            pad_along_dim = max(filt - stride, 0)
+        else:
+            pad_along_dim = max(filt - (size % stride), 0)
+        pad_before = pad_along_dim // 2
+        pad_after = pad_along_dim - pad_before
+        pad_amts.append((pad_before, pad_after))
+    # Do the actual padding
+    return jnp.pad(
+        x,
+        pad_amts,
+        mode="wrap" if pad_type == "circular" else "constant",
+    )
+
+
+class EasyPadConv(eqx.Module):
+    conv_op: eqx.nn.Conv
+    padding_type: str = eqx.static_field()
+
+    def __init__(
+        self,
+        num_spatial_dims: int,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int|Sequence[int],
+        stride: int = 1,
+        padding: typing.Literal["same", "valid", "circular"] = "valid",
+        dilation: int | Sequence[int] = 1,
+        groups: int = 1,
+        use_bias: bool = True,
+        *,
+        key = "jax.random.PRNGKey",
+    ):
+        self.padding_type = padding
+        self.conv_op = eqx.nn.Conv(
+            num_spatial_dims=num_spatial_dims,
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=0,
+            dilation=dilation,
+            groups=groups,
+            use_bias=use_bias,
+            key=key,
+        )
+
+    def __call__(self, x: Array, *, key: Array|None = None):
+        # Do pre-padding if needed
+        padded = _do_pad_input(
+            x,
+            pad_type=self.padding_type,
+            strides=self.conv_op.stride,
+            filter_sizes=self.conv_op.kernel_size,
+            n_spatial_dims=self.conv_op.num_spatial_dims,
+            dilations=self.conv_op.dilation,
+        )
+        return self.conv_op(padded)
