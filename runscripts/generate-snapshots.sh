c2059d721b370e41dc30b8f0c059a51f8164a9b0
Karl Otness <karl.otness@nyu.edu>
2022-10-24 13:48:41 -0400

    Remove hard-coded paths to scratch directory for data.

--- runscripts/generate-snapshots.sh
+++ runscripts/generate-snapshots.sh
@@ -1,2 +1,2 @@
 export JAX_ENABLE_X64=True
-sbatch --wrap="singularity exec --nv /scratch/kto236/closure/closure.sif python generate_data.py /scratch/kto236/closure/snap_data/train/ qg_snap 0 --num_trajs=5000" --job-name="qg-snap-gen" --time="12:00:00" --cpus-per-task=1 --mem="4G" --gres=gpu:1
+sbatch --wrap='singularity exec --nv ${SCRATCH}/closure/closure.sif python generate_data.py ${SCRATCH}/closure/snap_data/train/ qg_snap 0 --num_trajs=5000' --job-name="qg-snap-gen" --time="12:00:00" --cpus-per-task=1 --mem="4G" --gres=gpu:1
