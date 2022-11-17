9d0775a9763549a3e38996170475752ca1bef420
Karl Otness <karl.otness@nyu.edu>
2022-11-17 15:23:34 -0500

    Update data generation to only produce coarsening with operator 1.

--- runscripts/generate-data.sh
+++ runscripts/generate-data.sh
@@ -1,4 +1,4 @@
 export JAX_ENABLE_X64=True
-sbatch --wrap='singularity exec --nv ${SCRATCH}/closure/closure.sif python generate_data.py ${SCRATCH}/closure/data/train/ qg 0 --num_trajs=100' --job-name="qg-gen" --time="12:00:00" --cpus-per-task=2 --mem="15G" --gres=gpu:1
-sbatch --wrap='singularity exec --nv ${SCRATCH}/closure/closure.sif python generate_data.py ${SCRATCH}/closure/data/val/ qg 1 --num_trajs=3' --job-name="qg-gen" --time="8:00:00" --cpus-per-task=2 --mem="15G" --gres=gpu:1
-sbatch --wrap='singularity exec --nv ${SCRATCH}/closure/closure.sif python generate_data.py ${SCRATCH}/closure/data/test/ qg 2 --num_trajs=10' --job-name="qg-gen" --time="8:00:00" --cpus-per-task=2 --mem="15G" --gres=gpu:1
+sbatch --wrap='singularity exec --nv ${SCRATCH}/closure/closure.sif python generate_data.py ${SCRATCH}/closure/data/train/ qg 0 --num_trajs=100 --coarse_op op1' --job-name="qg-gen" --time="12:00:00" --cpus-per-task=2 --mem="15G" --gres=gpu:1
+sbatch --wrap='singularity exec --nv ${SCRATCH}/closure/closure.sif python generate_data.py ${SCRATCH}/closure/data/val/ qg 1 --num_trajs=3 --coarse_op op1' --job-name="qg-gen" --time="8:00:00" --cpus-per-task=2 --mem="15G" --gres=gpu:1
+sbatch --wrap='singularity exec --nv ${SCRATCH}/closure/closure.sif python generate_data.py ${SCRATCH}/closure/data/test/ qg 2 --num_trajs=10 --coarse_op op1' --job-name="qg-gen" --time="8:00:00" --cpus-per-task=2 --mem="15G" --gres=gpu:1
