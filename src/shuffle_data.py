a2f2db0e2bc7482d87a56bd6b39ec93533ca96d2
Karl Otness <karl.otness@nyu.edu>
2022-11-18 18:00:53 -0500

    Add script to pre-shuffle training sets.

---
+++ src/shuffle_data.py
@@ -0,0 +1,96 @@
+# Pre-shuffle data for faster loading as snapshots (slow to pre-process, then faster to load)
+# Handles fields q and q_total_forcing (includes references for steps and traj). No other data copied
+# Intended to be applied to training set only
+import argparse
+import pathlib
+import random
+import numpy as np
+import h5py
+import re
+import logging
+import math
+import utils
+
+parser = argparse.ArgumentParser(description="Pre-shuffle data for faster training")
+parser.add_argument("data_set", type=str, help="Directory containing original data to shuffle")
+parser.add_argument("--seed", type=int, default=None, help="Seed for shuffling")
+parser.add_argument("--log_level", type=str, help="Level for logger", default="info", choices=["debug", "info", "warning", "error", "critical"])
+
+
+CHUNK_SIZE = 1000
+FIELD_Q_RE = re.compile(rf"^traj\d{{5}}_q$")
+FIELD_Q_FORCING_RE = re.compile(rf"^traj\d{{5}}_q_total_forcing$")
+
+
+def main():
+    args = parser.parse_args()
+    data_dir = pathlib.Path(args.data_set)
+    utils.set_up_logging(level=args.log_level, out_file=data_dir/"shuffle_run.log")
+    logger = logging.getLogger("main")
+    logger.info("Arguments: %s", vars(args))
+    git_info = utils.get_git_info(base_logger=logger)
+    if git_info is not None:
+        logger.info(
+            "Running on commit %s (%s worktree)",
+            git_info.hash,
+            "clean" if git_info.clean_worktree else "dirty"
+        )
+    in_path = data_dir / "data.hdf5"
+    out_path = data_dir / "shuffled.hdf5"
+    if args.seed is None:
+        seed = random.SystemRandom().randint(0, 2**32)
+    else:
+        seed = args.seed
+    logger.info("Using seed %d", seed)
+    rng = np.random.default_rng(seed=seed)
+    with h5py.File(in_path, "r") as in_data:
+        # Compute number of trajectories and steps
+        num_trajs = sum(1 for k in in_data["trajs"].keys() if FIELD_Q_RE.match(k))
+        num_steps = in_data["trajs"]["traj00000_q"].shape[0]
+        total_steps = num_trajs * num_steps
+        logger.info("Shuffling %d trajs of %d steps -> %d total items", num_trajs, num_steps, total_steps)
+        # Shuffle indices
+        indices = np.arange(total_steps, dtype=np.uint64)
+        rng.shuffle(indices)
+        # Create compound dtypes
+        data_dtype = np.dtype(
+            [
+                ("q", in_data["trajs"]["traj00000_q"].dtype, in_data["trajs"]["traj00000_q"].shape[1:]),
+                ("q_total_forcing", in_data["trajs"]["traj00000_q_total_forcing"].dtype, in_data["trajs"]["traj00000_q_total_forcing"].shape[1:]),
+            ]
+        )
+        source_record_dtype = np.dtype(
+            [
+                ("traj", np.uint16),
+                ("step", np.uint32),
+            ]
+        )
+        # Create output record (empty)
+        with h5py.File(out_path, "w") as out_data:
+            data_dataset = out_data.create_dataset("shuffled", shape=(total_steps, ), dtype=data_dtype)
+            source_dataset = out_data.create_dataset("source", shape=(total_steps, ), dtype=source_record_dtype)
+            cursor = 0
+            steps = np.arange(num_steps, dtype=np.uint32)
+            # Write data chunked by permutation
+            for traj in range(num_trajs):
+                logger.info("Shuffling traj %d", traj)
+                q_in = in_data["trajs"][f"traj{traj:05d}_q"]
+                forcing_in = in_data["trajs"][f"traj{traj:05d}_q_total_forcing"]
+                for batch_start in range(0, num_steps, CHUNK_SIZE):
+                    batch_end = batch_start + CHUNK_SIZE
+                    q_chunk = q_in[batch_start:batch_end]
+                    forcing_chunk = forcing_in[batch_start:batch_end]
+                    batch_steps = steps[batch_start:batch_end]
+                    batch_size = q_chunk.shape[0]
+                    save_idxs = indices[cursor:cursor+batch_size]
+                    cursor += batch_size
+                    # Write output
+                    for idx, q, forcing, step in zip(save_idxs, q_chunk, forcing_chunk, batch_steps, strict=True):
+                        data_dataset[idx] = np.array([(q, forcing)], dtype=data_dtype)
+                        source_dataset[idx] = np.array([(np.uint16(traj), step)], dtype=source_record_dtype)
+
+    logger.info("Finished shuffling data")
+
+
+if __name__ == "__main__":
+    main()
