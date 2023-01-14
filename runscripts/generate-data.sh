056638736ce851de445345cc96fa4884354aaed2
Karl Otness <karl.otness@nyu.edu>
2023-01-14 04:34:24 -0500

    Adjust resource requests for generation and shuffling.

--- runscripts/generate-data.sh
+++ runscripts/generate-data.sh
@@ -14,15 +14,15 @@ function get_job_id() {
 export JAX_ENABLE_X64=True
 export JAX_DEFAULT_DTYPE_BITS=32
 
-TRAIN_OUT=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/data/train/' qg 0 --num_trajs=100 --coarse_op op1 --subsample 8" --job-name="qg-gen-train" --time="5:00:00" --cpus-per-task=2 --mem="15G" --gres=gpu:1)
-VAL_OUT=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/data/val/' qg 1 --num_trajs=3 --coarse_op op1 --subsample 8" --job-name="qg-gen-val" --time="2:00:00" --cpus-per-task=2 --mem="15G" --gres=gpu:1)
-TEST_OUT=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/data/test/' qg 2 --num_trajs=10 --coarse_op op1 --subsample 8" --job-name="qg-gen-test" --time="2:00:00" --cpus-per-task=2 --mem="15G" --gres=gpu:1)
+TRAIN_OUT=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/data/train/' qg 0 --num_trajs=100 --coarse_op op1 --subsample 8" --job-name="qg-gen-train" --time="8:00:00" --cpus-per-task=1 --mem="15G" --gres=gpu:1)
+VAL_OUT=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/data/val/' qg 1 --num_trajs=3 --coarse_op op1 --subsample 8" --job-name="qg-gen-val" --time="2:00:00" --cpus-per-task=1 --mem="15G" --gres=gpu:1)
+TEST_OUT=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/data/test/' qg 2 --num_trajs=10 --coarse_op op1 --subsample 8" --job-name="qg-gen-test" --time="2:00:00" --cpus-per-task=1 --mem="15G" --gres=gpu:1)
 
 TRAIN_JOBID=$(get_job_id "$TRAIN_OUT")
 VAL_JOBID=$(get_job_id "$VAL_OUT")
 TEST_JOBID=$(get_job_id "$TEST_OUT")
 
-SHUF_OUT=$(sbatch --wrap="singularity exec '${SCRATCH}/closure/closure.sif' python shuffle_data.py '${SCRATCH}/closure/data/train/'" --job-name="qg-shuf-train" --time="5:00:00" --cpus-per-task=8 --mem="8G" --dependency="afterok:$TRAIN_JOBID" --kill-on-invalid-dep=yes)
+SHUF_OUT=$(sbatch --wrap="singularity exec '${SCRATCH}/closure/closure.sif' python shuffle_data.py '${SCRATCH}/closure/data/train/'" --job-name="qg-shuf-train" --time="8:00:00" --cpus-per-task=8 --mem="8G" --dependency="afterok:$TRAIN_JOBID" --kill-on-invalid-dep=yes)
 SHUF_JOBID=$(get_job_id "$SHUF_OUT")
 
 echo "Generate train: $TRAIN_JOBID"
