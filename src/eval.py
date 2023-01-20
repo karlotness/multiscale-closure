1c09aea3f6a4da3b93da475fbb7d285fdcc27037
Karl Otness <karl.otness@nyu.edu>
2023-01-20 01:32:33 -0500

    Update evaluation to handle multiscale networks and large batches.

--- src/eval.py
+++ src/eval.py
@@ -18,7 +18,8 @@ import utils
 import math
 import dataclasses
 from methods.gz_fcnn import GZFCNN
-from train_sdegm import make_sampler, make_scalers
+from methods import ARCHITECTURES
+from train_sdegm import determine_required_fields, build_fixed_input_from_batch, make_scalers, make_coarseners, make_sampler
 import jax_utils
 
 
@@ -26,6 +27,7 @@ parser = argparse.ArgumentParser(description="Evaluate neural networks for closu
 parser.add_argument("net_dir", type=str, help="Directory with saved network weights")
 parser.add_argument("eval_set", type=str, help="Directory with evaluation examples")
 parser.add_argument("net_type", type=str, help="Which saved network weights to load")
+parser.add_argument("train_set", type=str, help="Directory with training data (used for scalers)")
 parser.add_argument("--log_level", type=str, help="Level for logger", default="info", choices=["debug", "info", "warning", "error", "critical"])
 parser.add_argument("--sampling_dt", type=float, default=0.01, help="Time step size for the SDE generating samples")
 parser.add_argument("--seed", type=int, default=None, help="Seed to use with RNG (if None, select automatically)")
@@ -35,23 +37,13 @@ parser.add_argument("--spectrum_seed", type=int, default=0, help="Seed used to d
 parser.add_argument("--num_mean_samples", type=int, default=10, help="Number of samples to compute mean of generated forcing")
 parser.add_argument("--spectrum_min_sample_step", type=int, default=0, help="Starting step index for sampling (can be set to exclude warmup)")
 
-def mse_loss(real_step, est_step):
-    err = jnp.abs(est_step - real_step)
-    return jnp.mean(err ** 2)
-
-
-def relerr_loss(real_step, est_step):
-    err = jnp.abs(est_step - real_step)
-    return jnp.mean(err / jnp.abs(real_step))
-
 
 def load_network(weight_file):
-    net = GZFCNN(
-        img_size=64,
-        n_layers_in=4,
-        n_layers_out=2,
-        key=jax.random.PRNGKey(0),
-    )
+    weight_file = pathlib.Path(weight_file)
+    # Load network info
+    with open(weight_file.parent / "network_info.json", "r", encoding="utf8") as net_info_file:
+        net_info = json.load(net_info_file)
+    net = ARCHITECTURES[net_info["arch"]](**net_info["args"], key=jax.random.PRNGKey(0))
     # Ensure all weights are float32
     def leaf_map(leaf):
         if isinstance(leaf, jnp.ndarray):
@@ -60,137 +52,114 @@ def load_network(weight_file):
             if leaf.dtype == jnp.dtype(jnp.complex128):
                 return leaf.astype(jnp.complex64)
         return leaf
-
     net = jax.tree_util.tree_map(leaf_map, net)
     net = eqx.tree_deserialise_leaves(weight_file, net)
-    return net
+    return net, net_info
 
 
-def make_eval_traj_computer(small_model, num_steps, observe_interval=1, q_param_func=None):
-    total_steps = num_steps * observe_interval
+def make_generative_sampler(net, num_mean_samples, sample_dt, scalers, coarseners, input_channels):
 
-    def state_scan(carry, x):
-        state, param_state = carry
-
-        def wrap_q_param(system_state):
-            nonlocal param_state
-            dq, param_state = q_param_func(system_state, param_state)
-            return dq
-
-        new_state = small_model.step_forward(state, q_param_func=wrap_q_param if q_param_func is not None else None)
-        return (new_state, param_state), new_state
-
-    def do_eval(initial_state, param_state=None):
-        (final_state, final_param_state), observed_states = jax_utils.strided_scan(
-            state_scan,
-            init=(initial_state, param_state),
-            xs=None,
-            length=num_steps,
-            stride=observe_interval,
+    def compute_samples_mean(batch, rng):
+        # Function of batch, net, rng
+        sample_fn = functools.partial(
+            make_sampler(
+                scalers=scalers,
+                coarseners=coarseners,
+                input_channels=input_channels,
+                dt=sample_dt,
+            ),
+            batch=batch,
+            net=net,
         )
-        return observed_states
+        rngs = jax.random.split(rng, num_mean_samples)
+        # Scan instead of vmap to save memory at the cost of parallelism
+        _carry, mean_samples = jax.lax.scan(
+            lambda _carry, x: (None, sample_fn(rng=x)[0]),
+            None,
+            rngs,
+            length=num_mean_samples,
+        )
+        sample = mean_samples[0]
+        mean = jnp.mean(mean_samples, axis=0)
+        return sample, mean
 
-    return do_eval
-
-
-def make_simple_sampler(net, dt=0.01, sample_shape=(2, 64, 64)):
-    q_scaler, forcing_scaler = make_scalers()
-    base_sampler = make_sampler(
-        dt=dt,
-        sample_shape=sample_shape,
-        q_scaler=q_scaler,
-        forcing_scaler=forcing_scaler,
-    )
-    train_state = types.SimpleNamespace(net=net)
-
-    def draw_sample(q, rng):
-        assert q.ndim == 3
-        # Expand to have batch and time dimensions
-        batch_q = jnp.expand_dims(q, (0, 1))
-        sample, _new_rng = base_sampler(train_state, batch_q, rng)
-        return jnp.squeeze(sample, 0)
-
-    return draw_sample
+    return compute_samples_mean
 
 
-def make_net_eval_traj_computer(net, small_model, num_steps, observe_interval=1, dt=0.01):
-    draw_samples = make_simple_sampler(
-        net=net,
-        dt=dt,
-    )
+def make_generative_stat_computer(net, num_mean_samples, sample_dt, scalers, coarseners, input_channels):
 
-    def q_param(state, key):
-        # Add batch and time dimensions
-        rng_ctr, rng = jax.random.split(key, 2)
-        # Draw single sample for q subgrid forcing
-        sample = draw_samples(state.q, rng)
-        return sample, rng_ctr
+    def map_gen_sampler(_carry, x):
+        batch_elem, rng = x
+        # Function of (batch, rng)
+        sampler_fn = make_generative_sampler(
+            net=net,
+            num_mean_samples=num_mean_samples,
+            sample_dt=sample_dt,
+            scalers=scalers,
+            coarseners=coarseners,
+            input_channels=input_channels,
+        )
+        sample, mean = sampler_fn(
+            batch=jax.tree_map(lambda arr: jnp.expand_dims(arr, 0), batch_elem),
+            rng=rng,
+        )
+        sample = jnp.squeeze(sample, 0)
+        mean = jnp.squeeze(mean, 0)
+        return None, (sample, mean)
 
-    return make_eval_traj_computer(
-        small_model=small_model,
-        num_steps=num_steps,
-        observe_interval=observe_interval,
-        q_param_func=q_param,
-    )
-
-
-def run_generative_offline_stats(net, eval_file, num_step_samples, spectrum_seed, num_mean_samples, base_logger, rng_ctr, sample_dt, min_sample_step=0):
-    sampler = make_simple_sampler(
-        net=net,
-        dt=sample_dt,
-    )
-
-    def inner_sample(q, rng_ctr):
-        rng = jax.random.split(rng_ctr, num_mean_samples)
-        return jax.vmap(sampler, in_axes=(None, 0))(q, rng)
-
-    @jax.jit
-    def compute_stats(q_stack, forcing_stack, rng_ctr):
-        assert q_stack.shape == forcing_stack.shape
-        num_qs = q_stack.shape[0]
-        rng = jax.random.split(rng_ctr, num_qs)
-        mean_samples = jax.vmap(inner_sample)(q_stack, rng)
-        # Mean samples has shape (num_qs, num_means, forcing...)
-        single_samples = mean_samples[:, 0]
-        means = jnp.mean(mean_samples, axis=1)
+    def compute_stats(batch, rng):
+        batch_size = jax.tree_util.tree_leaves(batch)[0].shape[0]
+        rng_ctr, *rngs = jax.random.split(rng, batch_size + 1)
+        rngs = jnp.stack(rngs)
+        _carry, (samples, means) = jax.lax.scan(
+            map_gen_sampler,
+            None,
+            (batch, rngs),
+        )
+        # Add time dimensions and compute stats
+        output_size = samples.shape[-1]
+        true_forcings = batch.q_total_forcings[output_size]
         stats = qg_spec_diag.subgrid_scores(
-            true=jnp.expand_dims(forcing_stack, 1),
+            true=jnp.expand_dims(true_forcings, 1),
             mean=jnp.expand_dims(means, 1),
-            gen=jnp.expand_dims(single_samples, 1),
+            gen=jnp.expand_dims(samples, 1),
         )
-        return stats
+        return stats, rng_ctr
 
+    return compute_stats
+
+
+def run_generative_offline_stats(net, eval_file, num_step_samples, spectrum_seed, num_mean_samples, base_logger, rng_ctr, sample_dt, scalers, coarseners, input_channels, output_size, min_sample_step=0):
     logger = base_logger.getChild("spec_offline")
     np_rng = np.random.default_rng(seed=spectrum_seed)
     logger.info("Starting to load evaluation data")
-    with SimpleQGLoader(eval_file, fields=["q", "q_total_forcing"]) as eval_loader:
+    with SimpleQGLoader(eval_file, fields=determine_required_fields(input_channels=input_channels, output_size=output_size)) as eval_loader:
         # First, sample the (traj, index) pairs used for "offline" statistics
         valid_step_count = eval_loader.num_steps - min_sample_step
         total_steps = eval_loader.num_trajs * valid_step_count
         sample_idxs = np_rng.integers(0, total_steps, size=num_step_samples, dtype=np.uint64)
         sample_trajs, sample_steps = np.divmod(sample_idxs, valid_step_count)
         sample_steps = sample_steps + min_sample_step
-        # For each of the selected steps and trajectories, load the q component and true forcing
-        # Ship them to the device and stack
-        q_stack = []
-        forcing_stack = []
-        for traj, step in zip(sample_trajs, sample_steps):
-            loaded = eval_loader.get_trajectory(traj=operator.index(traj), start=operator.index(step), end=operator.index(step) + 1)
-            q_stack.append(loaded.q)
-            forcing_stack.append(loaded.q_total_forcing)
-        loaded = None
-    q_stack = jnp.concatenate(q_stack, axis=0)
-    forcing_stack = jnp.concatenate(forcing_stack, axis=0)
+        # For each of these, load the relevant data and stack
+        batch = jax.tree_util.tree_map(
+            lambda *args: jnp.concatenate(args, axis=0),
+            *(eval_loader.get_trajectory(traj=operator.index(t), start=operator.index(s), end=operator.index(s)+1) for t, s in zip(sample_trajs, sample_steps, strict=True))
+        )
     logger.info("Finished loading evaluation data")
     # With loaded data on device, compute statistics
     logger.info("Starting statistics computation")
-    stats = jax.device_get(
-        compute_stats(
-            q_stack=q_stack,
-            forcing_stack=forcing_stack,
-            rng_ctr=rng_ctr,
+    stats_fn = jax.jit(
+        make_generative_stat_computer(
+            net=net,
+            num_mean_samples=num_mean_samples,
+            sample_dt=sample_dt,
+            scalers=scalers,
+            coarseners=coarseners,
+            input_channels=input_channels,
         )
     )
+    stats, rng_ctr = stats_fn(batch, rng_ctr)
+    stats = jax.device_get(stats)
     logger.info("Finished statistics computation")
     logger.info(f"Stat l2_mean: {stats.l2_mean:<15.5g}")
     logger.info(f"Stat l2_total: {stats.l2_total:<15.5g}")
@@ -211,7 +180,7 @@ def run_generative_offline_stats(net, eval_file, num_step_samples, spectrum_seed
         "trajs": sample_trajs,
         "steps": sample_steps,
     }
-    return json_results, bulk_results
+    return json_results, bulk_results, rng_ctr
 
 # TODO:
 # 1. Draw sample steps, compute forcings, compare with Pavel's offline metrics for true forcings
@@ -256,26 +225,44 @@ def main():
     rng_ctr = jax.random.PRNGKey(seed=np_rng.integers(2**32).item())
     # Find the training data file
     eval_file = pathlib.Path(args.eval_set) / "data.hdf5"
-    # Create the model
-    eval_small_model = qg_model_from_hdf5(file_path=eval_file, model="small")
     # Load the network
-    net = load_network(weight_file=weight_file)
+    net, net_info = load_network(weight_file=weight_file)
+    input_channels = net_info["input_channels"]
+    output_size = net_info["output_size"]
+    required_fields = determine_required_fields(
+        input_channels=input_channels,
+        output_size=output_size,
+    )
+    # Make scalers and coarseners
+    scalers = make_scalers(
+        source_data=pathlib.Path(args.train_set) / "shuffled.hdf5",
+        target_size=output_size,
+    )
+    coarseners = make_coarseners(
+        source_data=pathlib.Path(args.train_set) / "shuffled.hdf5",
+        target_size=output_size,
+        input_channels=input_channels,
+    )
+
     # Set up results files
     json_results = {}
     bulk_results = {}
 
     # GENERATIVE OFFLINE STATISTICS
     # Sample a few snapshots from the file, sample forcings and compute statistics vs the true forcing
-    rng_ctr, rng = jax.random.split(rng_ctr, 2)
-    gen_json_res, gen_bulk_res = run_generative_offline_stats(
+    gen_json_res, gen_bulk_res, rng_ctr= run_generative_offline_stats(
         net=net,
         eval_file=eval_file,
         num_step_samples=args.num_spectrum_samples,
         spectrum_seed=args.spectrum_seed,
         num_mean_samples=args.num_mean_samples,
         base_logger=logger,
-        rng_ctr=rng,
+        rng_ctr=rng_ctr,
         sample_dt=args.sampling_dt,
+        scalers=scalers,
+        coarseners=coarseners,
+        input_channels=input_channels,
+        output_size=output_size,
         min_sample_step=args.spectrum_min_sample_step,
     )
     # Add results to pending data
