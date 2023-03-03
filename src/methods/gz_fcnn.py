bc89bbec0c3fa7939373c1c8ca0d0df1093e3f2c
Karl Otness <karl.otness@nyu.edu>
2023-03-03 01:35:26 -0500

    Add support for layer norm to GZFCNN and combine implementations.

--- src/methods/gz_fcnn.py
+++ src/methods/gz_fcnn.py
@@ -8,41 +8,92 @@ import equinox as eqx
 from .eqx_modules import EasyPadConv
 
 
-class GZFCNN(eqx.Module):
+GZFCNN_FEATURES_KERNELS = {
+    "small": [
+        (128, (5, 5)),
+        (64, (5, 5)),
+        (32, (3, 3)),
+        (32, (3, 3)),
+        (32, (3, 3)),
+        (32, (3, 3)),
+        (32, (3, 3)),
+        ("layers_out", (3, 3)),
+    ],
+    "large": [
+        (128, (11, 11)),
+        (64, (11, 11)),
+        (32, (7, 7)),
+        (32, (7, 7)),
+        (32, (7, 7)),
+        (32, (7, 7)),
+        (32, (7, 7)),
+        ("layers_out", (7, 7)),
+    ],
+    "medium": [
+        (128, (9, 9)),
+        (64, (9, 9)),
+        (32, (5, 5)),
+        (32, (5, 5)),
+        (32, (5, 5)),
+        (32, (5, 5)),
+        (32, (5, 5)),
+        ("layers_out", (5, 5)),
+    ],
+}
+
+
+class BaseGZFCNN(eqx.Module):
     conv_seq: eqx.nn.Sequential
     img_size: int = eqx.static_field()
     n_layers_in: int = eqx.static_field()
     n_layers_out: int = eqx.static_field()
 
-    def __init__(self, img_size: int, n_layers_in: int, n_layers_out: int, padding: str = "circular", *, key: Array):
+    def __init__(
+        self,
+        img_size: int,
+        n_layers_in: int,
+        n_layers_out: int,
+        arch_size: str,
+        padding: str = "circular",
+        normalization: str | None = None,
+        *,
+        key: Array
+    ):
         self.img_size = img_size
         self.n_layers_in = n_layers_in
         self.n_layers_out = n_layers_out
 
-        features_kernels = [
-            (128, (5, 5)),
-            (64, (5, 5)),
-            (32, (3, 3)),
-            (32, (3, 3)),
-            (32, (3, 3)),
-            (32, (3, 3)),
-            (32, (3, 3)),
-            (self.n_layers_out, (3, 3)),
-        ]
+        features_kernels = GZFCNN_FEATURES_KERNELS[arch_size]
         ops = []
         prev_chans = self.n_layers_in
         keys = jax.random.split(key, len(features_kernels))
         for (feature, kern_size), conv_key in zip(features_kernels, keys, strict=True):
-            conv = EasyPadConv(
-                num_spatial_dims=2,
-                in_channels=prev_chans,
-                out_channels=feature,
-                kernel_size=kern_size,
-                padding=padding,
-                use_bias=True,
-                key=conv_key,
+            if feature == "layers_out":
+                feature = self.n_layers_out
+            ops.append(
+                EasyPadConv(
+                    num_spatial_dims=2,
+                    in_channels=prev_chans,
+                    out_channels=feature,
+                    kernel_size=kern_size,
+                    padding=padding,
+                    use_bias=True,
+                    key=conv_key,
+                )
             )
-            ops.extend([conv, eqx.nn.Lambda(jax.nn.relu)])
+            match normalization:
+                case None | "none":
+                    pass
+                case "layer":
+                    ops.append(
+                        eqx.nn.LayerNorm(
+                            shape=(feature, self.img_size, self.img_size),
+                            elementwise_affine=True,
+                        )
+                    )
+                case _:
+                    raise ValueError(f"invalid normalization choise {self.normalization}")
+            ops.append(eqx.nn.Lambda(jax.nn.relu))
             prev_chans = feature
         # Remove final activation
         ops.pop()
@@ -55,95 +106,37 @@ class GZFCNN(eqx.Module):
         return self.conv_seq(x, key=key)
 
 
-class LargeGZFCNN(eqx.Module):
-    conv_seq: eqx.nn.Sequential
-    img_size: int = eqx.static_field()
-    n_layers_in: int = eqx.static_field()
-    n_layers_out: int = eqx.static_field()
-
-    def __init__(self, img_size: int, n_layers_in: int, n_layers_out: int, padding: str = "circular", *, key: Array):
-        self.img_size = img_size
-        self.n_layers_in = n_layers_in
-        self.n_layers_out = n_layers_out
-
-        features_kernels = [
-            (128, (11, 11)),
-            (64, (11, 11)),
-            (32, (7, 7)),
-            (32, (7, 7)),
-            (32, (7, 7)),
-            (32, (7, 7)),
-            (32, (7, 7)),
-            (self.n_layers_out, (7, 7)),
-        ]
-        ops = []
-        prev_chans = self.n_layers_in
-        keys = jax.random.split(key, len(features_kernels))
-        for (feature, kern_size), conv_key in zip(features_kernels, keys, strict=True):
-            conv = EasyPadConv(
-                num_spatial_dims=2,
-                in_channels=prev_chans,
-                out_channels=feature,
-                kernel_size=kern_size,
-                padding=padding,
-                use_bias=True,
-                key=conv_key,
-            )
-            ops.extend([conv, eqx.nn.Lambda(jax.nn.relu)])
-            prev_chans = feature
-        # Remove final activation
-        ops.pop()
-        self.conv_seq = eqx.nn.Sequential(ops)
-
-    def __call__(self, x: Array, *, key: Array|None = None):
-        assert x.ndim == 3
-        assert x.shape[-2:] == (self.img_size, self.img_size)
-        assert x.shape[-3] == self.n_layers_in
-        return self.conv_seq(x, key=key)
+def GZFCNN(img_size, n_layers_in, n_layers_out, padding="circular", normalization=None, *, key):
+    return BaseGZFCNN(
+        img_size=img_size,
+        n_layers_in=n_layers_in,
+        n_layers_out=n_layers_out,
+        padding=padding,
+        arch_size="small",
+        normalization=normalization,
+        key=key,
+    )
 
 
-class MediumGZFCNN(eqx.Module):
-    conv_seq: eqx.nn.Sequential
-    img_size: int = eqx.static_field()
-    n_layers_in: int = eqx.static_field()
-    n_layers_out: int = eqx.static_field()
+def LargeGZFCNN(img_size, n_layers_in, n_layers_out, padding="circular", normalization=None, *, key):
+    return BaseGZFCNN(
+        img_size=img_size,
+        n_layers_in=n_layers_in,
+        n_layers_out=n_layers_out,
+        padding=padding,
+        arch_size="large",
+        normalization=normalization,
+        key=key,
+    )
 
-    def __init__(self, img_size: int, n_layers_in: int, n_layers_out: int, padding: str = "circular", *, key: Array):
-        self.img_size = img_size
-        self.n_layers_in = n_layers_in
-        self.n_layers_out = n_layers_out
 
-        features_kernels = [
-            (128, (9, 9)),
-            (64, (9, 9)),
-            (32, (5, 5)),
-            (32, (5, 5)),
-            (32, (5, 5)),
-            (32, (5, 5)),
-            (32, (5, 5)),
-            (self.n_layers_out, (5, 5)),
-        ]
-        ops = []
-        prev_chans = self.n_layers_in
-        keys = jax.random.split(key, len(features_kernels))
-        for (feature, kern_size), conv_key in zip(features_kernels, keys, strict=True):
-            conv = EasyPadConv(
-                num_spatial_dims=2,
-                in_channels=prev_chans,
-                out_channels=feature,
-                kernel_size=kern_size,
-                padding=padding,
-                use_bias=True,
-                key=conv_key,
-            )
-            ops.extend([conv, eqx.nn.Lambda(jax.nn.relu)])
-            prev_chans = feature
-        # Remove final activation
-        ops.pop()
-        self.conv_seq = eqx.nn.Sequential(ops)
-
-    def __call__(self, x: Array, *, key: Array|None = None):
-        assert x.ndim == 3
-        assert x.shape[-2:] == (self.img_size, self.img_size)
-        assert x.shape[-3] == self.n_layers_in
-        return self.conv_seq(x, key=key)
+def MediumGZFCNN(img_size, n_layers_in, n_layers_out, padding="circular", normalization=None, *, key):
+    return BaseGZFCNN(
+        img_size=img_size,
+        n_layers_in=n_layers_in,
+        n_layers_out=n_layers_out,
+        padding=padding,
+        arch_size="medium",
+        normalization=normalization,
+        key=key,
+    )
