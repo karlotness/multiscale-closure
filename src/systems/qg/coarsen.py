cfc7500996dabefa377fa85d65e0f064d14a2eee
Karl Otness <karl.otness@nyu.edu>
2023-03-15 02:22:18 -0400

    Revert "Port coarseners to public pyqg_jax package."
    This reverts commit ad5e3ad561efd9c16814e973c17bcc9079fc85c4.

--- src/systems/qg/coarsen.py
+++ src/systems/qg/coarsen.py
@@ -1,31 +1,111 @@
+import dataclasses
+import jax
 import jax.numpy as jnp
-import pyqg_jax
-from . import utils
+from jaxtyping import Array, Float
+import json
+from .kernel import _generic_rfftn, _generic_irfftn, register_dataclass_pytree
+from .qg_model import QGModel
+
+# Spectral filters as used in Ross, Li, Perezhogin, et al (2022)
+
+@register_dataclass_pytree
+@dataclasses.dataclass
+class PartialCoarsenedStep:
+    q: Array
+    dqhdt: Array
+    t: Array
+    tc: Array
+    ablevel: Array
+    q_total_forcing: Array
 
 
-def _generic_rfftn(a):
-    return jnp.fft.rfftn(a, axes=(-2, -1))
-
-
-def _generic_irfftn(a):
-    return jnp.fft.irfftn(a, axes=(-2, -1))
-
-
-def coarsen_model(big_model, small_nx):
-    assert big_model.nx == big_model.ny
-    assert small_nx < big_model.nx
-    assert small_nx % 2 == 0
-    model_args = utils.qg_model_to_args(big_model)
-    model_args["nx"] = small_nx
-    model_args["ny"] = small_ny
-    return pyqg_jax.qg_model.QGModel(**model_args)
-
+@register_dataclass_pytree
+@dataclasses.dataclass
+class PartialCoarsenedTraj:
+    q: Array
+    dqhdt: Array
+    dqhdt_p: Array
+    dqhdt_pp: Array
+    t: Array
+    tc: Array
+    ablevel: Array
+    q_total_forcing: Array
 
 class Coarsener:
     def __init__(self, big_model, small_nx):
+        assert big_model.nx == big_model.ny
+        assert small_nx < big_model.nx
+        assert small_nx % 2 == 0
+        params = json.loads(big_model.param_json())
+        params["nx"] = small_nx
+        params["ny"] = small_nx
         self.big_model = big_model
-        self.small_model = coarsen_model(self.big_model, small_nx)
+        self.small_model = QGModel.from_param_json(json.dumps(params))
         self.ratio = self.big_model.nx / self.small_model.nx
+
+    def _coarsen_step(self, big_state):
+        assert big_state.q.ndim == 3
+        assert big_state.q.shape == (self.big_model.nz, self.big_model.ny, self.big_model.nx)
+        # Coarsen q, dqhdt
+        q = self.coarsen(big_state.q)
+        dqhdt = self.coarsen(big_state.dqhdt)
+        # Copy other attributes
+        t = big_state.t
+        tc = big_state.tc
+        ablevel = big_state.ablevel
+        # Compute q total forcing
+        small_state = self.small_model.create_initial_state(jax.random.PRNGKey(0))
+        #  step 1: copy basic attributes
+        small_state.t = big_state.t
+        small_state.tc = big_state.tc
+        small_state.ablevel = big_state.ablevel
+        small_state.q = q
+        #  step 2: run part of time-stepping calculations
+        small_state = self.small_model.invert(small_state) # Recompute ph, u, v
+        small_state = self.small_model.do_advection(small_state) # Recompute uq, vq, dqhdt
+        small_state = self.small_model.do_friction(small_state) # Recompute dqhdt
+        #  step 3: q forcing is the subtraction of dqhdt
+        q_total_forcing = self._to_real(dqhdt) - self._to_real(small_state.dqhdt)
+        # Package values and return
+        return PartialCoarsenedStep(
+            q=q,
+            dqhdt=dqhdt,
+            t=t,
+            tc=tc,
+            ablevel=ablevel,
+            q_total_forcing=q_total_forcing,
+        )
+
+    def coarsen_traj(self, big_traj):
+        assert big_traj.q.ndim == 4
+        assert big_traj.q.shape[1:] == (self.big_model.nz, self.big_model.ny, self.big_model.nx)
+        # Coarsen each state
+        small_steps = jax.vmap(self._coarsen_step)(big_traj)
+        # Patch up the dqhdt_p and dqhdt_pp values
+        dqhdt_p = jnp.concatenate(
+            [
+                jnp.expand_dims(jnp.zeros_like(small_steps.dqhdt[0]), 0),
+                small_steps.dqhdt[:-1],
+            ]
+        )
+        dqhdt_pp = jnp.concatenate(
+            [
+                jnp.zeros_like(small_steps.dqhdt[:2]),
+                small_steps.dqhdt[:-2],
+            ]
+        )
+        small_traj = PartialCoarsenedTraj(
+            q=small_steps.q,
+            dqhdt=small_steps.dqhdt,
+            dqhdt_p=dqhdt_p,
+            dqhdt_pp=dqhdt_pp,
+            t=small_steps.t,
+            tc=small_steps.tc,
+            ablevel=small_steps.ablevel,
+            q_total_forcing=q_total_forcing,
+        )
+        # All done
+        return small_traj
 
     def coarsen(self, var):
         raise NotImplementedError("implement in a subclass")
--- src/systems/qg/utils.py
+++ src/systems/qg/utils.py
@@ -1,21 +1,166 @@
-def qg_model_to_args(model):
-    qg_args = {
-        "nx",
-        "ny",
-        "L",
-        "W",
-        "rek",
-        "filterfac",
-        "f",
-        "g",
-        "beta",
-        "rd",
-        "delta",
-        "H1",
-        "U1",
-        "U2",
-        "precision",
-    }
-    return {
-        arg: getattr(model, arg) for arg in qg_args
-    }
+import functools
+import dataclasses
+import jax
+import jax.numpy as jnp
+from .kernel import PseudoSpectralKernelState
+
+
+def make_basic_rollout(model, num_steps):
+
+    def do_steps(carry_state, _y):
+        next_state = model.step_forward(carry_state)
+        return next_state, carry_state
+
+    def gen_traj(first_step):
+        _last_step, steps = jax.lax.scan(do_steps, first_step, None, length=num_steps)
+        return steps
+
+    return gen_traj
+
+
+def make_basic_final_step(model, num_steps):
+
+    def do_steps(carry_state, _y):
+        next_state = model.step_forward(carry_state)
+        return next_state, None
+
+    def gen_final_step(first_step):
+        last_step, _carries = jax.lax.scan(do_steps, first_step, None, length=num_steps)
+        return last_step
+
+    return gen_final_step
+
+
+def make_gen_traj(model, num_steps):
+
+    def gen_traj(rng):
+        init = model.create_initial_state(rng)
+        return make_basic_rollout(model, num_steps)(init)
+
+    return gen_traj
+
+
+def make_gen_final_step(model, num_steps):
+
+    def gen_final_step(rng):
+        init = model.create_initial_state(rng)
+        return make_basic_final_step(model, num_steps)(init)
+
+    return gen_final_step
+
+
+def slice_kernel_state(state, slicer):
+    data_fields = frozenset(f.name for f in dataclasses.fields(PseudoSpectralKernelState))
+    return PseudoSpectralKernelState(**{k: getattr(state, k)[slicer] for k in data_fields})
+
+
+def get_online_rollout(start_state, num_steps, apply_fn, params, small_model, memory_init_fn, batch_stats, param_type="uv", train=False, scan_fn=jax.lax.scan):
+    match param_type:
+        case "uv":
+            memory = memory_init_fn(params, start_state.u, start_state.v)
+        case "q":
+            memory = memory_init_fn(params, start_state.q, start_state.u, start_state.v)
+        case _:
+            raise ValueError(f"invalid parameterization type {param_type}")
+
+    def do_steps(carry_state, _):
+        old_state, memory, stats = carry_state
+
+        def apply_net_uv(state):
+            nonlocal memory, stats
+            net_param = {"params": params, "batch_stats": stats}
+            if train:
+                (sx, sy, memory), new_stats = apply_fn(net_param, state.u, state.v, memory, train, mutable=["batch_stats"])
+                stats = new_stats["batch_stats"]
+            else:
+                sx, sy, memory = apply_fn(net_param, state.u, state.v, memory, train)
+            return sx, sy
+
+        def apply_net_q(state):
+            nonlocal memory, stats
+            net_param = {"params": params, "batch_stats": stats}
+            if train:
+                (dq, memory), new_stats = apply_fn(net_param, state.q, state.u, state.v, memory, train, mutable=["batch_stats"])
+                stats = new_stats["batch_stats"]
+            else:
+                dq, memory = apply_fn(net_param, state.q, state.u, state.v, memory, train)
+            return dq
+
+        match param_type:
+            case "uv":
+                kw = {"uv_param_func": apply_net_uv}
+            case "q":
+                kw = {"q_param_func": apply_net_q}
+            case _:
+                raise ValueError(f"invalid parameterization type {param_type}")
+
+        new_state = small_model.step_forward(old_state, **kw)
+        return (new_state, memory, stats), new_state
+
+    (_last_step, last_memory, last_batch_stats), new_states = scan_fn(
+        do_steps,
+        (start_state, memory, batch_stats),
+        None,
+        length=num_steps,
+    )
+    if train:
+        return new_states, last_memory, last_batch_stats
+    else:
+        return new_states, last_memory
+
+
+def get_online_batch_loss(real_traj, apply_fn, params, small_model, loss_fn, memory_init_fn, batch_stats, param_type="uv", train=False, scan_fn=jax.lax.scan):
+    first_step = slice_kernel_state(real_traj, 0)
+
+    match param_type:
+        case "uv":
+            memory = memory_init_fn(params, first_step.u, first_step.v)
+        case "q":
+            memory = memory_init_fn(params, first_step.q, first_step.u, first_step.v)
+        case _:
+            raise ValueError(f"invalid parameterization type {param_type}")
+
+    def do_steps(carry_state, true_step):
+        old_state, memory, stats = carry_state
+
+        def apply_net_uv(state):
+            nonlocal memory, stats
+            net_param = {"params": params, "batch_stats": stats}
+            if train:
+                (sx, sy, memory), new_stats = apply_fn(net_param, state.u, state.v, memory, train, mutable=["batch_stats"])
+                stats = new_stats["batch_stats"]
+            else:
+                sx, sy, memory = apply_fn(net_param, state.u, state.v, memory, train)
+            return sx, sy
+
+        def apply_net_q(state):
+            nonlocal memory, stats
+            net_param = {"params": params, "batch_stats": stats}
+            if train:
+                (dq, memory), new_stats = apply_fn(net_param, state.q, state.u, state.v, memory, train, mutable=["batch_stats"])
+                stats = new_stats["batch_stats"]
+            else:
+                dq, memory = apply_fn(net_param, state.q, state.u, state.v, memory, train)
+            return dq
+
+        match param_type:
+            case "uv":
+                kw = {"uv_param_func": apply_net_uv}
+            case "q":
+                kw = {"q_param_func": apply_net_q}
+            case _:
+                raise ValueError(f"invalid parameterization type {param_type}")
+
+        new_state = small_model.step_forward(old_state, **kw)
+        loss = loss_fn(real_step=true_step.q, est_step=new_state.q)
+        return (new_state, memory, stats), loss
+
+    (_last_step, _last_memory, last_batch_stats), losses = scan_fn(
+        do_steps,
+        (first_step, memory, batch_stats),
+        slice_kernel_state(real_traj, slice(1, None)),
+    )
+    if train:
+        return losses, last_batch_stats
+    else:
+        return losses
