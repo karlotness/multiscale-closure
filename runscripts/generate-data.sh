96b41a2f660d140b721936d6949223a940e750a8
Karl Otness <karl.otness@nyu.edu>
2023-06-20 03:18:37 -0400

    Split data generation into two hour chunks of work.

--- runscripts/generate-data.sh
+++ runscripts/generate-data.sh
@@ -18,12 +18,27 @@ readonly COARSEN_OP='op1'
 
 for expansion_level in '0.05' '0.10' '0.20' '0.50' '0.75'; do
     level_underscore=$(echo "$expansion_level" | tr '.' '_')
-    DATA_DIR="data-rand-eddytojet/factor-${level_underscore}"
+    DATA_DIR="data-rand-detect/factor-${level_underscore}"
     data_config="rand-eddy-to-jet-${expansion_level}"
 
     mkdir -p "${SCRATCH}/closure/${DATA_DIR}"
     # Train
-    TRAIN_OUT=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/${DATA_DIR}/train/' qg 0 --config ${data_config} --num_trajs=275 --coarse_op ${COARSEN_OP} --subsample 1000 --small_size 64 48 --dt 3600.0 --tmax 470160000.0 --twarmup 155520000.0" --job-name="qg-gen-train" --time="8:00:00" --cpus-per-task=1 --mem="20G" --gpus=1 --partition=gpu)
+    TRAIN_OUT1=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/${DATA_DIR}/train/' qg 0 --config ${data_config} --num_trajs=275 --traj_slice '0:60' --coarse_op ${COARSEN_OP} --subsample 1000 --small_size 64 48 --dt 3600.0 --tmax 470160000.0 --twarmup 155520000.0" --job-name="qg-gen-train" --time="4:00:00" --cpus-per-task=1 --mem="20G" --gpus=1 --partition=gpu)
+    TRAIN_JOBID1=$(get_job_id "$TRAIN_OUT1")
+    sleep 0.5
+    TRAIN_OUT2=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/${DATA_DIR}/train/' qg 0 --config ${data_config} --num_trajs=275 --traj_slice '60:120' --coarse_op ${COARSEN_OP} --subsample 1000 --small_size 64 48 --dt 3600.0 --tmax 470160000.0 --twarmup 155520000.0" --job-name="qg-gen-train" --time="4:00:00" --cpus-per-task=1 --mem="20G" --gpus=1 --partition=gpu)
+    TRAIN_JOBID2=$(get_job_id "$TRAIN_OUT2")
+    sleep 0.5
+    TRAIN_OUT3=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/${DATA_DIR}/train/' qg 0 --config ${data_config} --num_trajs=275 --traj_slice '120:180' --coarse_op ${COARSEN_OP} --subsample 1000 --small_size 64 48 --dt 3600.0 --tmax 470160000.0 --twarmup 155520000.0" --job-name="qg-gen-train" --time="4:00:00" --cpus-per-task=1 --mem="20G" --gpus=1 --partition=gpu)
+    TRAIN_JOBID3=$(get_job_id "$TRAIN_OUT3")
+    sleep 0.5
+    TRAIN_OUT4=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/${DATA_DIR}/train/' qg 0 --config ${data_config} --num_trajs=275 --traj_slice '180:240' --coarse_op ${COARSEN_OP} --subsample 1000 --small_size 64 48 --dt 3600.0 --tmax 470160000.0 --twarmup 155520000.0" --job-name="qg-gen-train" --time="4:00:00" --cpus-per-task=1 --mem="20G" --gpus=1 --partition=gpu)
+    TRAIN_JOBID4=$(get_job_id "$TRAIN_OUT4")
+    sleep 0.5
+    TRAIN_OUT5=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/${DATA_DIR}/train/' qg 0 --config ${data_config} --num_trajs=275 --traj_slice '240:' --coarse_op ${COARSEN_OP} --subsample 1000 --small_size 64 48 --dt 3600.0 --tmax 470160000.0 --twarmup 155520000.0" --job-name="qg-gen-train" --time="4:00:00" --cpus-per-task=1 --mem="20G" --gpus=1 --partition=gpu)
+    TRAIN_JOBID5=$(get_job_id "$TRAIN_OUT5")
+    sleep 0.5
+    TRAIN_OUT=$(sbatch --wrap="singularity exec '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/${DATA_DIR}/train/${COARSEN_OP}/' combine_qg_slice" --job-name="qg-combine" --time="8:00:00" --cpus-per-task=2 --mem="20G" --dependency="afterok:$TRAIN_JOBID1:$TRAIN_JOBID2:$TRAIN_JOBID3:$TRAIN_JOBID4:$TRAIN_JOBID5" --kill-on-invalid-dep=yes)
     sleep 0.5
     # Val
     VAL_OUT=$(sbatch --wrap="singularity exec --nv '${SCRATCH}/closure/closure.sif' python generate_data.py '${SCRATCH}/closure/${DATA_DIR}/val/' qg 1 --config ${data_config} --num_trajs=4 --coarse_op ${COARSEN_OP} --subsample 1000 --small_size 64 48 --dt 3600.0 --tmax 470160000.0 --twarmup 155520000.0" --job-name="qg-gen-val" --time="1:00:00" --cpus-per-task=1 --mem="15G" --gpus=1 --partition=gpu)
@@ -42,9 +57,14 @@ for expansion_level in '0.05' '0.10' '0.20' '0.50' '0.75'; do
     sleep 0.5
 
     echo "EddyToJet data expansion: ${expansion_level}"
-    echo "Generate train: $TRAIN_JOBID"
-    echo "Generate val:   $VAL_JOBID"
-    echo "Generate test:  $TEST_JOBID"
-    echo "Shuffle train:  $SHUF_JOBID"
+    echo "Generate train slice 1: $TRAIN_JOBID1"
+    echo "Generate train slice 2: $TRAIN_JOBID2"
+    echo "Generate train slice 3: $TRAIN_JOBID3"
+    echo "Generate train slice 4: $TRAIN_JOBID4"
+    echo "Generate train slice 5: $TRAIN_JOBID5"
+    echo "Generate train:         $TRAIN_JOBID"
+    echo "Generate val:           $VAL_JOBID"
+    echo "Generate test:          $TEST_JOBID"
+    echo "Shuffle train:          $SHUF_JOBID"
 
 done
