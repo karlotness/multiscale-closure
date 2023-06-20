4aa500c4e825af748f8d5b06de5a308ea391a0d8
Karl Otness <karl.otness@nyu.edu>
2023-06-20 11:51:17 -0400

    Add scripts to train networks with varying system params.

---
+++ runscripts/eval-online.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#SBATCH --job-name=eval-online
+#SBATCH --time=3:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=15GB
+#SBATCH --gpus=1
+#SBATCH --partition=gpu
+
+set -euo pipefail
+
+if [[ $# -lt 4 ]]; then
+    echo 'ERROR: Insufficient parameters for online evaluation'
+    echo 'Usage multi-train-cnn.sh OUT_DIR TRAIN_FACTOR NET_TYPE NET_DIRS...'
+    exit 1
+fi
+
+readonly OUT_DIR="$1"
+readonly TRAIN_FACTOR="$2"
+readonly NET_TYPE="$3"
+readonly NET_DIRS=( "${@:4}" )
+readonly TRAIN_FACTOR_UNDERSCORE=$(echo "$TRAIN_FACTOR" | tr '.' '_')
+
+# Make Bash more strict
+shopt -s failglob
+set -euo pipefail
+
+# Constants
+source params.sh
+
+# Clone Repository
+mkdir -p "$CHECKOUT_DIR"
+git clone "$ORIGIN_REPO_DIR" "$CHECKOUT_DIR"
+
+# Prepare output
+mkdir -p "$OUT_DIR"
+
+# Run
+export JAX_ENABLE_X64=True
+export JAX_DEFAULT_DTYPE_BITS=32
+singularity run --nv "$SINGULARITY_CONTAINER" \
+            python "${CHECKOUT_DIR}/src/online_ensemble_compare.py" \
+            --net_type="$NET_TYPE" \
+            "$OUT_DIR" \
+            "${SCRATCH}/closure/data-rand-eddytojet/factor-${TRAIN_FACTOR_UNDERSCORE}/test/${COARSE_OP}/" \
+            "${NET_DIRS[@]}"
---
+++ runscripts/train-randfactor.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#SBATCH --job-name=train-randfactor
+#SBATCH --time=5:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=11GB
+#SBATCH --gpus=1
+#SBATCH --partition=gpu
+
+# Reproduce Ross22 paper training loop
+# Using data sets with varying system parameters
+
+set -euo pipefail
+
+if [[ $# -lt 2 ]]; then
+    echo 'ERROR: Insufficient parameters for training'
+    echo 'Usage multi-train-cnn.sh OUT_DIR TRAIN_FACTOR'
+    exit 1
+fi
+
+readonly OUT_DIR="$1"
+readonly TRAIN_FACTOR="$2"
+readonly TRAIN_FACTOR_UNDERSCORE=$(echo "$TRAIN_FACTOR" | tr '.' '_')
+
+# Make Bash more strict
+shopt -s failglob
+set -euo pipefail
+
+# Constants
+source params.sh
+
+# Clone Repository
+mkdir -p "$CHECKOUT_DIR"
+git clone "$ORIGIN_REPO_DIR" "$CHECKOUT_DIR"
+
+# Prepare output
+mkdir -p "$OUT_DIR"
+
+# Run
+export JAX_ENABLE_X64=True
+export JAX_DEFAULT_DTYPE_BITS=32
+singularity run --nv "$SINGULARITY_CONTAINER" \
+            python "${CHECKOUT_DIR}/src/train.py" "$OUT_DIR" "${SCRATCH}/closure/data-rand-eddytojet/factor-${TRAIN_FACTOR_UNDERSCORE}/train/${COARSE_OP}/" "${SCRATCH}/closure/data-rand-eddytojet/factor-${TRAIN_FACTOR_UNDERSCORE}/val/${COARSE_OP}/" \
+            --optimizer=adam \
+            --batch_size=64 \
+            --num_epochs=50 \
+            --batches_per_epoch=374 \
+            --loader_chunk_size=23925 \
+            --num_val_samples=100 \
+            --val_interval=1 \
+            --save_interval=1 \
+            --lr=0.001 \
+            --end_lr=0.001 \
+            --lr_schedule=ross22 \
+            --architecture='gz-fcnn-v1' \
+            --input_channels "q_64" "rek_64" "delta_64" "beta_64" \
+            --output_channels "q_total_forcing_64"
