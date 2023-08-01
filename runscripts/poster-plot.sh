7fc2c41ce4b8e6d47b93b32c5eaa1f4edfe409b6
Karl Otness <karl.otness@nyu.edu>
2023-08-01 04:30:24 -0400

    Add scripts to launch poster evaluations.

--- runscripts/poster-plot.sh
+++ runscripts/poster-plot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=eval-plots
-#SBATCH --time=6:00:00
+#SBATCH --time=10:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=15GB
 #SBATCH --gpus=1
@@ -11,13 +11,13 @@ set -euo pipefail
 
 if [[ $# -lt 3 ]]; then
     echo 'ERROR: Insufficient parameters for online evaluation'
-    echo 'Usage multi-train-cnn.sh OUT_FILE EVAL_FILE NET_DIRS...'
+    echo 'Usage multi-train-cnn.sh OUT_FILE EVAL_FILE NET_FILES...'
     exit 1
 fi
 
 readonly OUT_FILE="$1"
 readonly EVAL_FILE="$2"
-readonly NET_DIRS=( "${@:3}" )
+readonly NET_FILES=( "${@:3}" )
 
 # Make Bash more strict
 shopt -s failglob
@@ -40,4 +40,4 @@ singularity run --nv "$SINGULARITY_CONTAINER" \
             python "${CHECKOUT_DIR}/src/online_data_eval.py" \
             "$OUT_FILE" \
             "$EVAL_FILE" \
-            "${NET_DIRS[@]}"
+            "${NET_FILES[@]}"
