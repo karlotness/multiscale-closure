6d1c3450ce8d094b7c14c8ecf52d63efad210b06
Karl Otness <karl.otness@nyu.edu>
2023-08-01 03:46:24 -0400

    Add script to execute poster plot data job.

---
+++ runscripts/poster-plot.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#SBATCH --job-name=eval-plots
+#SBATCH --time=6:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=15GB
+#SBATCH --gpus=1
+#SBATCH --partition=gpu
+
+set -euo pipefail
+
+if [[ $# -lt 3 ]]; then
+    echo 'ERROR: Insufficient parameters for online evaluation'
+    echo 'Usage multi-train-cnn.sh OUT_FILE EVAL_FILE NET_DIRS...'
+    exit 1
+fi
+
+readonly OUT_FILE="$1"
+readonly EVAL_FILE="$2"
+readonly NET_DIRS=( "${@:3}" )
+
+# Make Bash more strict
+shopt -s failglob
+set -euo pipefail
+
+# Constants
+source params.sh
+
+# Clone Repository
+mkdir -p "$CHECKOUT_DIR"
+git clone "$ORIGIN_REPO_DIR" "$CHECKOUT_DIR"
+
+# Prepare output
+mkdir -p "$(dirname "$OUT_FILE")"
+
+# Run
+export JAX_ENABLE_X64=True
+export JAX_DEFAULT_DTYPE_BITS=32
+singularity run --nv "$SINGULARITY_CONTAINER" \
+            python "${CHECKOUT_DIR}/src/online_data_eval.py" \
+            "$OUT_FILE" \
+            "$EVAL_FILE" \
+            "${NET_DIRS[@]}"
