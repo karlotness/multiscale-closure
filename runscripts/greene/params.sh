2a91c6f67d9c107b3daa25f11842beff31fab06f
Karl Otness <karl.otness@nyu.edu>
2023-09-08 02:24:46 -0400

    Set up custom TMPDIR in runs on Greene.

--- runscripts/greene/params.sh
+++ runscripts/greene/params.sh
@@ -2,10 +2,19 @@
 readonly DATA_SUBDIR='data-nowarmup'
 readonly COARSE_OP='op1'
 
+readonly JOBTMP_DIR=$(mktemp --tmpdir="$SLURM_JOBTMP" -d "${USER}_job${SLURM_JOBID}_XXXXXXXXXX")
+
+function cleanup_jobtmp_dir() {
+    rm -rf "$JOBTMP_DIR"
+}
+trap cleanup_jobtmp_dir EXIT
+
+export TMPDIR="$JOBTMP_DIR"
+
 # Run parameters
 readonly SINGULARITY_CONTAINER="${SCRATCH}/closure/closure.sif"
 readonly ORIGIN_REPO_DIR="${HOME}/repos/closure.git"
-readonly CHECKOUT_DIR="${SLURM_JOBTMP}/Closure/"
+readonly CHECKOUT_DIR="${JOBTMP_DIR}/Closure/"
 readonly TRAIN_DATA_DIR="${SCRATCH}/closure/${DATA_SUBDIR}/train/${COARSE_OP}/"
 readonly VAL_DATA_DIR="${SCRATCH}/closure/${DATA_SUBDIR}/val/${COARSE_OP}/"
 readonly EVAL_DATA_DIR="${SCRATCH}/closure/${DATA_SUBDIR}/test/${COARSE_OP}/"
