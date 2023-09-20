b27c8b78f4e6318e11ffebbaee9d719435d37c11
Karl Otness <karl.otness@nyu.edu>
2023-09-20 14:24:56 -0400

    Add network stacking GZFCNN with skip connections.

--- src/methods/__init__.py
+++ src/methods/__init__.py
@@ -1,10 +1,12 @@
 import importlib
 
-def make_module_factory(module, class_name):
+def make_module_factory(module, class_name, fixed_args=None):
+    if fixed_args is None:
+        fixed_args = {}
 
     def make_module(*args, **kwargs):
         mod_name = importlib.import_module(f".{module}", __name__)
-        return getattr(mod_name, class_name)(*args, **kwargs)
+        return getattr(mod_name, class_name)(*args, **kwargs, **fixed_args)
 
     return make_module
 
@@ -14,4 +16,8 @@ ARCHITECTURES = {
     "gz-fcnn-v1-large": make_module_factory("gz_fcnn", "LargeGZFCNN"),
     "gz-fcnn-v1-medium": make_module_factory("gz_fcnn", "MediumGZFCNN"),
     "unet-v1": make_module_factory("basic_unet", "BasicUNetV1"),
+    "stacked-gz-fcnn-v1-d2": make_module_factory("stacked_gz_fcnn", "StackedGZFCNN", fixed_args={"depth": 2}),
+    "stacked-gz-fcnn-v1-d3": make_module_factory("stacked_gz_fcnn", "StackedGZFCNN", fixed_args={"depth": 3}),
+    "stacked-gz-fcnn-v1-medium-d2": make_module_factory("stacked_gz_fcnn", "MediumStackedGZFCNN", fixed_args={"depth": 2}),
+    "stacked-gz-fcnn-v1-medium-d3": make_module_factory("stacked_gz_fcnn", "MediumStackedGZFCNN", fixed_args={"depth": 3}),
 }
---
+++ src/methods/stacked_gz_fcnn.py
@@ -0,0 +1,79 @@
+import collections.abc
+import jax
+import jax.numpy as jnp
+import equinox as eqx
+from .gz_fcnn import GZFCNN, MediumGZFCNN, BaseGZFCNN
+
+
+class BaseStackedGZFCNN(eqx.Module):
+    seq: collections.abc.Sequence[eqx.Module]
+    img_size: int = eqx.field(static=True)
+    n_layers_in: int = eqx.field(static=True)
+    n_layers_out: int = eqx.field(static=True)
+
+    def __init__(
+        self,
+        img_size: int,
+        n_layers_in: int,
+        n_layers_out: int,
+        depth: int,
+        padding: str = "circular",
+        normalization: str | None = None,
+        zero_mean: bool = False,
+        fcnn_class: type[BaseGZFCNN] = GZFCNN,
+        *,
+        key: jax.Array,
+    ):
+        self.seq = []
+        self.img_size = img_size
+        self.n_layers_in = n_layers_in
+        self.n_layers_out = n_layers_out
+        prev_layers = n_layers_in
+        keys = jax.random.split(key, depth)
+        for net_key in keys:
+            self.seq.append(
+                fcnn_class(
+                    img_size=img_size,
+                    n_layers_in=prev_layers,
+                    n_layers_out=n_layers_out,
+                    padding=padding,
+                    normalization=normalization,
+                    zero_mean=zero_mean,
+                    key=net_key,
+                )
+            )
+            prev_layers += self.seq[-1].n_layers_out
+
+    def __call__(self, x: jax.Array, *, key: jax.Array|None = None):
+        inputs = [x]
+        for net in self.seq:
+            result = net(jnp.concatenate(inputs, axis=0))
+            inputs.append(result)
+        return result
+
+
+def StackedGZFCNN(img_size, n_layers_in, n_layers_out, depth, padding="circular", normalization=None, zero_mean=False, *, key):
+    return BaseStackedGZFCNN(
+        img_size=img_size,
+        n_layers_in=n_layers_in,
+        n_layers_out=n_layers_out,
+        depth=depth,
+        padding=padding,
+        fcnn_class=GZFCNN,
+        normalization=normalization,
+        zero_mean=zero_mean,
+        key=key,
+    )
+
+def MediumStackedGZFCNN(img_size, n_layers_in, n_layers_out, depth, padding="circular", normalization=None, zero_mean=False, *, key):
+    return BaseStackedGZFCNN(
+        img_size=img_size,
+        n_layers_in=n_layers_in,
+        n_layers_out=n_layers_out,
+        depth=depth,
+        padding=padding,
+        fcnn_class=MediumGZFCNN,
+        normalization=normalization,
+        zero_mean=zero_mean,
+        key=key,
+    )
